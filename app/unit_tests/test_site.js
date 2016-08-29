589c1edbe295589eeba7ceeb2506ac3918babd05
unknown <cspeter@csz104.win2k.cs.ust.hk>
2016-08-29 17:55:43 +0800

    Initial commit of teamform-seed

---
+++ app/unit_tests/test_site.js
@@ -0,0 +1,16 @@
+describe('Test site.js', function() {
+	
+   //
+   // Example: A test case of getRandomIntInclusive
+   //
+   describe('getRandomIntInclusive Coverage Test', function() {
+
+	  it('value within 1 to 3', function() {
+	  	var value = getRandomIntInclusive(1, 3);
+	  	expect( value>=1 && value <= 3 ).toEqual(true);
+	  });
+
+   });
+
+
+});
