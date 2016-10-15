393f99eaacc6df4332319aa0971514a87d8c4ade
Billy Li <billyli@me.com>
2016-10-16 03:47:52 +0800

    update deploy script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd /home/ec2-user/teamform-seed/
+chown -R ec2-user:ec2-user ../**
 npm install --only=dev
 npm install --only=prod
-sudo npm install forever bower rimraf webpack -g
+npm install forever bower rimraf webpack -g
 npm run build
 
 sudo forever start /home/ec2-user/teamform-seed/app.js
