207b01ffd30344b7d2f0d0eefe72bcf1eeccc3c3
Gilbert Man <Gilbert Man>
2016-10-30 11:46:40 +0800

    Refine login function and add uid to firebase database

--- app/js/login.js
+++ app/js/login.js
@@ -1,77 +1,123 @@
-
-
 $(document).ready(function(){
-
-    initalizeFirebase();
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: "+profile.providerId);
-      console.log("Provider-specific UID: "+profile.uid);
-      console.log("Name: "+profile.displayName);
-      console.log("Email: "+profile.email);
-      console.log("Photo URL: "+profile.photoURL);
-      $('#user_name').html(profile.displayName);
-      $('#user_name2').text(profile.displayName);
-      $('.user-image').attr('src',profile.photoURL);
-      $('.img-circle').attr('src',profile.photoURL);
-    });
-
-    } else {
-      // No user is signed in.
-    }
-    });
-
-
-
 
 });
 
-function onSignIn(googleUser) {
-  console.log('Google Auth Response', googleUser);
-  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-    unsubscribe();
-    // Check if we are already signed-in Firebase with the correct user.
-    if (!isUserEqual(googleUser, firebaseUser)) {
-      // Build Firebase credential with the Google ID token.
-      var credential = firebase.auth.GoogleAuthProvider.credential(
-          googleUser.getAuthResponse().id_token);
-      // Sign in with credential from the Google user.
-      firebase.auth().signInWithCredential(credential).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-    } else {
-      console.log('User already signed-in Firebase.');
-    }
+angular.module('teamform-index-app', ['firebase'])
+.controller('LoginCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+
+	// TODO: implementation of AdminCtrl
+
+	// Initialize $scope.param as an empty JSON object
+	$scope.param = {};
+
+	// Call Firebase initialization code defined in site.js
+  initalizeFirebase();
+	var id;
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    user.providerData.forEach(function (profile) {
+    $('#user_name').html(profile.displayName);
+    $('#user_name2').text(profile.displayName);
+    $('.user-image').attr('src',profile.photoURL);
+    $('.img-circle').attr('src',profile.photoURL);
+
+
+  });
+    $('.g-signin2').css('display', 'none');
+  } else {
+    // No user is signed in.
+  }
   });
 
 
+	function onSignIn(googleUser) {
+	  console.log('Google Auth Response', googleUser);
+	  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+	  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+	    unsubscribe();
+	    // Check if we are already signed-in Firebase with the correct user.
+	    if (!isUserEqual(googleUser, firebaseUser)) {
+	      // Build Firebase credential with the Google ID token.
+	      var credential = firebase.auth.GoogleAuthProvider.credential(
+	          googleUser.getAuthResponse().id_token);
+	      // Sign in with credential from the Google user.
+	      firebase.auth().signInWithCredential(credential).catch(function(error) {
+	        // Handle Errors here.
+	        var errorCode = error.code;
+	        var errorMessage = error.message;
+	        // The email of the user's account used.
+	        var email = error.email;
+	        // The firebase.auth.AuthCredential type that was used.
+	        var credential = error.credential;
+	        // ...
 
-}
-function isUserEqual(googleUser, firebaseUser)
-{
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()) {
-        // We don't need to reauth the Firebase connection.
-        return true;
-      }
-    }
-  }
-  return false;
-}
+	      });
+
+				firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+					// User is signed in.
+					user.providerData.forEach(function (profile) {
+					var refPath;
+					id = firebase.auth().currentUser.uid;
+					refPath = "/users/" + id;
+					// Link and sync a firebase object
+						$scope[id] = [];
+						$scope[id] = $firebaseObject(firebase.database().ref(refPath));
+						$scope[id].$loaded()
+						.then( function(data) {
+							console.log('abc');
+						$scope[id].name = profile.displayName;
+						$scope[id].$save();
+						})
+						.catch(function(error) {
+							// Database connection error handling...
+							//console.error("Error:", error);
+						});
+
+				});
+				} else {
+					// No user is signed in.
+				}
+				});
+
+	    }
+			else {
+	      console.log('User already sign in');
+	    }
+	  });
+
+	}
+
+
+	function isUserEqual(googleUser, firebaseUser)
+	{
+	  if (firebaseUser) {
+	    var providerData = firebaseUser.providerData;
+	    for (var i = 0; i < providerData.length; i++) {
+
+	      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+	          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+	        // We don't need to reauth the Firebase connection.
+	        return true;
+	      }
+	    }
+	  }
+		return false;
+
+	}
+
+  	$scope.saveFunc = function() {
+  	$scope[id].$save();
+  	// Finally, go back to the front-end
+  	window.location.href= "index.html";
+	}
+ 	window.onSignIn = onSignIn;
+
+}]);
+
+
 
 function signOut()
 {
