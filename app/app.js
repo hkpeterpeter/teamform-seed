6a995153d6b21034010aebba6c56e03a11e59e55
Fish <fishyu624@gmail.com>
2016-11-02 04:26:02 +0800

    Register function (complete)

--- app/app.js
+++ app/app.js
@@ -2,16 +2,14 @@ var app = angular.module("myApp", ["firebase"]);
 
 // and use it in our controller
 app.controller("RegCtrl", ["$scope", '$firebaseAuth', function($scope, $firebaseAuth) {
-    var ref = new Firebase("https://comp3111-bb108.firebaseio.com/");
-    $scope.authObj = $firebaseAuth();
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth(firebase.auth());
     $scope.signUp = function() {
-        $scope.authObj.$createUser({
-            email: $scope.input.email,
-            password: $scope.input.password
-        }).then(function(userData) {
+        auth.$createUserWithEmailAndPassword($scope.input.email,$scope.input.password)
+        .then(function(userData) {
             console.log("User " + userData.uid + " created successfully!");
 
-            return $scope.authObj.$authWithPassword({
+            return auth.$signInWithEmailAndPassword({
                 email: $scope.input.email,
                 password: $scope.input.password
             });
