06f230c13dae8d6e2154c558f1aacb1c4dddfe28
Brandon Ng <brndnng@gmail.com>
2016-11-01 23:45:09 +0800

    Added login function
    Added login/register, logout functions

---
+++ app/js/check_login.js
@@ -0,0 +1,42 @@
+initalizeFirebase();
+      var initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            user.getToken().then(function(accessToken) {
+              document.getElementById('sign-in-status').textContent = 'Signed in';
+              document.getElementById('user-info').style.display = 'block';
+              document.getElementById('name').textContent = user.displayName;
+              document.getElementById('email').textContent = user.email;
+              document.getElementById('account-details').textContent = JSON.stringify({
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                photoURL: photoURL,
+                uid: uid,
+                accessToken: accessToken,
+                providerData: providerData
+              }, null, '  ');
+            });
+          } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('account-details').textContent = '';
+          }
+        }, function(error) {
+          console.log(error);
+        });
+        document.getElementById('sign-out').addEventListener('click', function() {
+           firebase.auth().signOut();
+  });
+      };
+
+      window.addEventListener('load', function() {
+        initApp()
+      });
