afd87ef8fcdc271b469e4fc0073cd2db86c47f2d
He Zhou <hezhou556@gmail.com>
2016-11-02 10:57:52 +0800

    Create the HTML of personal dashboard and its JS

---
+++ app/Samuel-personalDashboard/personal-dashboard-main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Samuel on 2/11/2016.
+ */
+
+var dasApp = angular.module("dashboard",[]);
+dasApp.controller("dashboardController",function($scope){
+    $scope.username = "Samuel He";
+});
