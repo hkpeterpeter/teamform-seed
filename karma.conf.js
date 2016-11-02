7457fdf71a9039e0d53efbc84c603fb5efcbf52c
Keith Hon <keithhon@Keiths-MacBook-Pro-2.local>
2016-11-02 20:16:45 +0800

    update .ignore, copied package.json

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,80 @@
-//jshint strict: false
+// Karma configuration
+// Generated on Wed Sep 28 2016 10:04:06 GMT+0800 (HKT)
+
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
-	frameworks: ['jasmine'],
-    files: [
-	  'lib/jquery.min.js',
-      'lib/angular.min.js',
-      'lib/angular-route.min.js',
-      'lib/angular-mocks.js',
-	  'lib/firebase.js',
-	  'lib/angularfire.min.js',	  
-	  'js/*.js',
-      'unit_tests/*.js'	  
-    ],
-	exclude: [
-	],
-	preprocessors: {	 	
-		 'js/site.js' : ['coverage'],		
-		 'js/index.js' : ['coverage'],
-		 'js/admin.js' : ['coverage'],
-		 'js/team.js' : ['coverage'],
-		 'js/member.js' : ['coverage']
-	},
-	reporters: ['progress', 'coverage'],
-	coverageReporter: {
-			type: 'html',
-			dir: 'coverage/',
-			subdir: '.'
-	},
-	port: 8080,
-	colors: true,
-    browsers: ['Chrome'],
-	singleRun: true,
-    plugins: [
-      'karma-chrome-launcher',      
-      'karma-jasmine',
-	  'karma-coverage'
-    ]    
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-  });
-};
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'node_modules/angular/angular.js',
+        'node_modules/angular-mocks/angular-mocks.js',
+        'js/*.js',
+        'test/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        'js/*.js': ['coverage']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter:{
+
+        type: 'html',
+        dir: 'coverage/'
+
+    },
+
+
+    // web server port
+    port: 8080,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
