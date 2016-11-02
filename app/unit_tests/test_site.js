cb7b094b99f5175526369b4986b8870365996cc2
jerryleungwh <jerryleungwh@gmail.com>
2016-11-02 11:40:49 +0800

    ~added test cases for changeMinSize and changeMaxSize ~

--- app/unit_tests/test_site.js
+++ app/unit_tests/test_site.js
@@ -11,6 +11,36 @@ describe('Test site.js', function() {
 	  });
 
    });
+   
 
 
+describe('teamform-admin-app', function() {
+
+	var createController, $scope;
+
+	beforeEach(inject(function($rootScope, _$controller_){
+        $scope = $rootScope.$new();
+		
+		createController = function() {
+            return $controller('teamform-admin-app', {
+                '$scope': $scope
+            });
+        };
+	}));
+	
+
+
+	  it('change min size to 3', function() {
+	  	$scope.changeMinTeamSize(3);
+	  	expect($scope.param.minTeamSize).toBe(3);
+	  });
+	  
+	  it('change max size to 10', function() {
+	  	$scope.changeMaxTeamSize(10);
+	  	expect($scope.param.minTeamSize).toBe(10);
+	  });
+	  
+	  
+});   
+   
 });
