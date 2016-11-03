9ae8645dc2ad4fc315ee4e3a937ff809df96524e
RonaldShum <khshumab@connect.ust.hk>
2016-11-03 18:21:14 +0800

    basic login finished

--- app/js/index.js
+++ app/js/index.js
@@ -30,3 +30,63 @@ $(document).ready(function(){
 
 
 });
+
+angular.module('index-app', ['firebase'])
+.controller('LoginCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+	//init firebase
+	initalizeFirebase();
+	$scope.txtEmail = '';
+	$scope.txtPassword = '';
+	$scope.showLogout  = false;
+	$scope.displayEmail = '';
+	
+	//login function
+	$scope.login = function(){
+		const email = $scope.txtEmail;
+		const pass = $scope.txtPassword;
+		console.log(email);
+		console.log(pass);
+		const auth = firebase.auth();
+		//Sign in
+		const promise = auth.signInWithEmailAndPassword(email,pass);
+		promsie.catch(e => console.log(e.message));
+	}
+
+	//signup function
+	$scope.signup = function(){
+		const email = $scope.txtEmail;
+		const pass = $scope.txtPassword;
+		console.log(email);
+		console.log(pass);
+		const auth = firebase.auth();
+		//Sign up
+		const promise = auth.createUserWithEmailAndPassword(email,pass);
+		promsie.catch(e => console.log(e.message));
+	}
+
+	//logout function
+	$scope.logout = function(){
+		firebase.auth().signOut();
+	}
+
+	//Change showLogout
+	$scope.changeShowLogout = function(bool){
+		$scope.showLogout = bool;
+	}
+
+	firebase.auth().onAuthStateChanged(user => {
+		if(user){
+			console.log(user);
+			$scope.changeShowLogout(true);
+			console.log($scope.showLogout);
+			$scope.displayEmail = user.email;
+			$scope.$apply();
+		}else{
+			console.log('not log in');
+			$scope.changeShowLogout(false);
+			$scope.$apply();
+		}
+	})
+
+
+}]);
