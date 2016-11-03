9e5482423d598edbf20be19cafbfc80c7c421518
Keith Hon <keithhon@Keiths-MacBook-Pro-2.local>
2016-11-03 12:16:59 +0800

    replace jQuery.js and bootstrap.js by angular ui-bootstrap instead

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,22 @@
-var app = angular.module('teamform', []);
+var app = angular.module('teamform', ['ui.bootstrap']);
 app.controller('myCtrl', function($scope) {
 
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+
+    // toggle wrapper to show and hide the left sidebar
+    $scope.toggleWrapper = function() {
+
+        if ($scope.isToggled != "toggled") {
+
+                $scope.isToggled = "toggled";
+
+        } else {
+
+            $scope.isToggled = "nottoggled";
+
+        }
+
+    }
+
 });
