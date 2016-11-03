089ad15e57e2d7c33b28510520c885a1f2b6f25d
He Zhou <hezhou556@gmail.com>
2016-11-03 16:17:11 +0800

    Add the prototype of skill manager

--- app/Samuel-personalDashboard/personal-dashboard-main.js
+++ app/Samuel-personalDashboard/personal-dashboard-main.js
@@ -2,7 +2,14 @@
  * Created by Samuel on 2/11/2016.
  */
 
-var dasApp = angular.module("dashboard",[]);
-dasApp.controller("dashboardController",function($scope){
+var dasApp = angular.module("dashboard",['firebase']);
+dasApp.controller("dashboardController",function($scope, $firebaseArray){
     $scope.username = "Samuel He";
+    var firebaseRef = firebase.database().ref().child("skills");
+    $scope.skills = $firebaseArray(firebaseRef);
+
+    $scope.receiveNewSikll = function(){
+        $scope.skills.$add($scope.newSkill);
+        $scope.newSkill = '';
+    };
 });
