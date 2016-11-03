6f811e6e6b404f6698d5619ba95e6b26564f644a
a4n0nym0u5 <hades94720@gmail.com>
2016-11-04 01:04:50 +0800

    not completed yet

--- app/app.route.js
+++ app/app.route.js
@@ -1,4 +1,4 @@
-var app = angular.module("RouteApp", ["ui.router"], ["firebase"]);
+var app = angular.module("RouteApp", ["ui.router","firebase"]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
@@ -9,6 +9,42 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('home', {
             url: '/home',
             templateUrl: 'main.html'
+        })
+        
+        .state('login', {
+            url: '/login',
+            templateUrl: 'login.html'
         });
         
+        
 });
+
+app.controller("Signin", ["$scope", '$firebaseAuth', function($scope, $firebaseAuth) {
+   
+     
+   var provider = new firebase.auth.FacebookAuthProvider();
+
+   $scope.login = function() {
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    });
+     
+     };
+   
+   
+   
+}]);
