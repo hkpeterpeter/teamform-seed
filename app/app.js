a028ca1d0516e2827e363e2f20b69f54e0f69010
a4n0nym0u5 <hades94720@gmail.com>
2016-11-04 13:17:21 +0800

    Integrate temporary

--- app/app.js
+++ app/app.js
@@ -1,6 +1,29 @@
-var app = angular.module("myApp", ["firebase"]);
+var app = angular.module("RouteApp", ["ui.router","firebase"]);
 
-// and use it in our controller
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'main.html'
+        })
+        
+        .state('login', {
+            url: '/login',
+            templateUrl: 'login.html',
+            controller: 'Signin'
+        })
+        
+         .state('register', {
+            url: '/register',
+            templateUrl: 'register.html',
+            controller: 'AuthCtrl'
+        });
+        
+});
 app.controller("AuthCtrl", ["$scope", '$firebaseAuth', function($scope, $firebaseAuth) {
     var ref = firebase.database().ref();
     var auth = $firebaseAuth(firebase.auth());
@@ -26,4 +49,34 @@ app.controller("AuthCtrl", ["$scope", '$firebaseAuth', function($scope, $firebas
 			LoginMessage = "Authentication failed:" + error;
 		});
     };
+}]);
+
+app.controller("Signin", ["$scope", '$firebaseAuth', function($scope, $firebaseAuth) {
+   
+     
+   var provider = new firebase.auth.FacebookAuthProvider();
+
+   $scope.login = function() {
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    });
+     
+     };
+   
+   
+   
 }]);
