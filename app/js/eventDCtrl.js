2df706144c69a6b8e5689b8760a2fa037cfcd213
bliuag <bliuag@connect.ust.hk>
2016-11-04 17:44:15 +0800

    implementation of basic event detail page

--- app/js/eventDCtrl.js
+++ app/js/eventDCtrl.js
@@ -8,13 +8,15 @@ app.controller("eventDCtrl",
         //initialize
         $scope.isManaging = false;
         $scope.userdata = "";
+        $scope.selectTeam = false;
+        personToBeAdded="";
         Auth.$onAuthStateChanged(function(authData) {
             $scope.userData = authData;
 
             //get role of user
             ref = firebase.database().ref("users/" + $scope.userData.uid + "/writable");
             $scope.obj = $firebaseObject(ref);
-            console.log($scope.obj);
+            // console.log($scope.obj);
             if (authData) {
 
             } else console.log("signed out");
@@ -24,18 +26,33 @@ app.controller("eventDCtrl",
         $scope.eventID = "-KVcVgeRPOOdr-vUyDLV"; // eventID = $stateParams.eid;
         eventRef = firebase.database().ref("events/" + $scope.eventID);
         $scope.eventObj = $firebaseObject(eventRef);
-
+        $scope.eventObj.$loaded().then(function(data){
+            console.log($scope.eventObj.eventInfo);
+        })
 
         //functions
         $scope.manage = function() {
             $scope.isManaging = !$scope.isManaging;
-        }
+        };
 
         //Debug
 
-
-
         $scope.createEventList = Helper.debug.createEventList;
+        $scope.deleteTeam = function(key){
+            Helper.deleteTeam($scope.eventID,key);
+        };
+        $scope.addToTeam = function(id){
+            $scope.selectTeam=!$scope.selectTeam;
+            personToBeAdded=id;
+
+        }
+        $scope.toTeam=function(key){
+            helper.addPersonToTeam(personToBeAdded,$scope.eventID,key);
+            $scope.selectTeam=false;
+            console.log(key);
+        }
+        console.log("test");
+        
 
 
     }
--- app/js/helper.js
+++ app/js/helper.js
@@ -6,16 +6,61 @@ app.factory("Helper", function($firebaseArray, $firebaseObject) {
     helper = {};
     helper.debug = {};
     helper.addPersonToTeam = function(uid, eventID, teamID) {
-
+        ref=firebase.database().ref("events/"+eventID);
+        eventObj=$firebaseObject(ref);
+        eventObj.$loaded().then(function(data){
+            delete eventObj.tba[uid];
+            console.log(eventObj);
+            if(eventObj.teams[teamID].members===undefined)
+                eventObj.teams[teamID].members={};
+            eventObj.teams[teamID].members[uid]=uid;
+            eventObj.$save();
+        });
+        ref=firebase.database().ref("users/"+uid+"/writable/"+eventID);
+        eventRec=$firebaseObject(ref);
+        eventRec.$loaded().then(function(data){
+            eventRec.position = "member";
+            eventRec.team = teamID;
+            eventRec.$save();
+        })
     }
     helper.deletePersonFromTeam = function(uid, eventID, teamID) {
-
+        ref=firebase.database().ref("users/"+uid+"/writable/"+eventID);
+        record=$firebaseObject(ref);
+        record.$loaded().then(function(data){
+            record.position="tba";
+            teamID=record.team;
+            record.team="";
+            record.$save();
+            teamRef=firebase.database().ref("events/"+eventID+"/teams/"+teamID);
+            team=$firebaseObject(teamRef);
+            team.$loaded().then(function(data){
+                for(key in team.member)
+                {
+                    if(team.member[key]==uid)
+                        delete team.member[key];
+                }
+                team.$save;
+            });
+            helper.pushNotificationTo(uid, eventID, "You have been deleted from your team");
+        })
     }
     helper.createTeam = function(uid, eventID, team) {
 
     }
-    helper.deleteTeam = function(uid, eventID, teamID) {
-
+    helper.deleteTeam = function(eventID, teamID) {
+        ref=firebase.database().ref("events/"+eventID+"/teams/"+teamID);
+        team=$firebaseObject(ref);
+        team.$loaded().then(function(data){
+            for(key in team.member)
+                {
+                    id=team.member[key];
+                    console.log(id);
+                    helper.pushNotificationTo(id, eventID, "Your team has been deleted");
+                    helper.deletePersonFromTeam(id,eventID,teamID);
+                }
+                team.$remove();
+        });
     }
     helper.createEvent = function(uid, event) {
 
@@ -23,7 +68,7 @@ app.factory("Helper", function($firebaseArray, $firebaseObject) {
     helper.deleteEvent = function(uid, eventID) {
 
     }
-    helper.pushNotificationTo = function(toUid, msg) {
+    helper.pushNotificationTo = function(toUid, eventID, msg) {
 
     }
     helper.sendInvitationTo = function(toUid, eventID, teamID) {
