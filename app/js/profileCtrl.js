280a44bbe542599b79a7e291d465323264ba7702
Mark <hcaiaa@connect.ust.hk>
2016-11-05 14:48:14 +0800

    profile modified

--- app/js/profileCtrl.js
+++ app/js/profileCtrl.js
@@ -2,29 +2,32 @@
 app.controller("profileCtrl", 
 
 	// Implementation the todoCtrl 
-	function($scope, Auth, $firebaseArray, $firebaseObject,$window) {
+	function($scope, Auth, $firebaseArray, $firebaseObject,$window, $stateParams) {
 		Auth.$onAuthStateChanged(function(authData){
 			//initialize
-			$scope.authData = authData;
-			ref = firebase.database().ref("users/"+$scope.authData.uid+"/readOnly/info");
-			profile_info = $firebaseObject(ref);
-			profile_info.$loaded().then(function(){
-				console.log(profile_info);
-				$scope.profile_name  = profile_info.name;
-				$scope.profile_age  = profile_info.age ;
-				$scope.profile_company = profile_info.company;
-			});
-			$scope.profile_readOnly = true;
-			console.log($window.location.href);
-			var start_pos = $window.location.href.lastIndexOf("profile/");
-			var end_pos = $window.location.href.length;
-			var id = $window.location.href.slice(start_pos+8,end_pos);
-			if (id != $scope.authData.uid)  $scope.button_visible = true;
-			else $scope.button_visible = false;
-			if (authData) console.log(authData);
+			if (authData) {
+				$scope.authData = authData;
+				ref = firebase.database().ref("users/"+$scope.authData.uid+"/readOnly/info");
+				$scope.profile_info = $firebaseObject(ref);
+				// profile_info.$loaded().then(function(){
+				// 	console.log(profile_info);
+				// 	$scope.profile_name  = profile_info.name;
+				// 	$scope.profile_age  = profile_info.age ;
+				// 	$scope.profile_company = profile_info.company;
+				// });
+				$scope.profile_readOnly = true;
+				// console.log($window.location.href);
+				// var start_pos = $window.location.href.lastIndexOf("profile/");
+				// var end_pos = $window.location.href.length;
+				// var id = $window.location.href.slice(start_pos+8,end_pos);
+				var id = $stateParams.uid;
+				if (id != $scope.authData.uid)  $scope.button_visible = false;
+				else $scope.button_visible = true;
+				console.log(authData);
+			}
 			else {
 				console.log("signed out");
-				$window.location.href = '/';
+				// $window.location.href = '/';
 			}
 		});
 		$scope.button_name = "EDIT";
@@ -34,14 +37,14 @@ app.controller("profileCtrl",
 				$scope.button_name = "SAVE";
 			}
 			else{
-				ref = firebase.database().ref("users/"+$scope.authData.uid+"/readOnly/info");
-				profile_info = $firebaseObject(ref);
-				profile_info.$loaded();
-				profile_info.name = $scope.profile_name;
-				profile_info.age = $scope.profile_age;
-				profile_info.company = $scope.profile_company;
-				profile_info.$save().then(function(){
-					console.log(profile_info);
+				// ref = firebase.database().ref("users/"+$scope.authData.uid+"/readOnly/info");
+				// profile_info = $firebaseObject(ref);
+				// profile_info.$loaded();
+				// profile_info.name = $scope.profile_name;
+				// profile_info.age = $scope.profile_age;
+				// profile_info.company = $scope.profile_company;
+				$scope.profile_info.$save().then(function(){
+					console.log($scope.profile_info);
 				});
 				$scope.profile_readOnly=true;
 				$scope.button_name = "EDIT";
