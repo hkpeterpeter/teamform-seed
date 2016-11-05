f936d4220479fc4e1c07c37e340eb164fda1f77d
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-11-05 21:40:01 +0800

    Add Components

---
+++ app/zhuxinyu/js/components/js/form.js
@@ -0,0 +1,53 @@
+var app = angular.module('eventSearch', []);
+    
+    app.controller('myCtrl', function($scope) {
+        $scope.team_min = 0;
+        $scope.team_max = 0;
+    });
+    
+    app.directive("simpleField",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "component/simple_field.html"
+        };
+    });
+    
+    app.directive("textareaField",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "component/textarea_field.html"
+        };
+    });
+    
+    app.directive("imageUpload",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "component/image_upload.html"
+        };
+    });
+    
+    app.directive("eventForm",function(){
+        return {
+            restrict: "E",
+            templateUrl: "component/event_form.html"
+        };
+    });
+    
+    app.directive("teamForm",function(){
+        return {
+            restrict: "E",
+            templateUrl: "component/team_form.html"
+        };
+    });
--- app/zhuxinyu/myctrl.js
+++ app/zhuxinyu/myctrl.js
@@ -1,45 +1,120 @@
-var myapp=angular.module('eventSearch', []);
-myapp.controller('ctroller', ['$scope', function($scope){
-	$scope.event={
-		name:"",
-		invite:[],
-		adm:"",
-		detail:"Event Detail"
-	};
-
-	$scope.createflip=function(){
-		if($scope.event.name!=""){
-			document.getElementById('myflipper').classList.toggle('flipped');
-			console.log($scope.event.name);
-		}
-	}
-	
+var myapp = angular.module('eventSearch', []);
+myapp.controller('ctroller', ['$scope', function($scope) {
+    $scope.event = {
+        name: "",
+        invite: [],
+        adm: "",
+        detail: "Event Detail"
+    };
+    $scope.createflip = function() {
+        if ($scope.event.name != "") {
+            document.getElementById('myflipper').classList.toggle('flipped');
+            console.log($scope.event.name);
+        }
+    }
 }]);
-myapp.directive('eventCard', function(){
-	// Runs during compile
-
-	return {
-		// name: '',
-		// priority: 1,
-		// terminal: true,
-		 scope: {
-		 	eventTitle:"@etitle",
-		 	eventPicture:"@epicture",
-		 	eadmin:"@eadmin",
-		 	eGroupSize:"@eGroupSize",
-		 	eSkill:"@eSkill"
-		 }, // {} = isolate, true = child, false/undefined = no change
-		// controller: function($scope, $element, $attrs, $transclude) {},
-		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-		 //template: '',
-		templateUrl: 'eventCard.html',
-		replace:true,
-		// replace: true,
-		// transclude: true,
-		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-		link: function($scope, iElm, iAttrs, controller) {
-			
-		}
-	};
+myapp.directive('eventSearchPanel', function() {
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        // scope: {
+        //     backgroundImage:"@backgroundImage"
+        // },
+        // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        //template: '',
+        templateUrl: 'js/components/eventSearchPanel/eventSearchPanel.html',
+        replace: false,
+        // replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, iElm, iAttrs, controller) {}
+    };
 });
+myapp.directive('eventCard', function() {
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        scope: {
+            eventTitle: "@etitle",
+            eventPicture: "@epicture",
+            eadmin: "@eadmin",
+            eGroupSize: "@eGroupSize",
+            eSkill: "@eSkill"
+        }, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        //template: '',
+        templateUrl: 'js/components/eventCard/eventCard.html',
+        replace: true,
+        // replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, iElm, iAttrs, controller) {}
+    };
+});
+
+
+ myapp.directive("subcan",function(){
+        return {
+            restrict: "E",
+            templateUrl: "js/components/submitCancelPanel/subcan.html"
+        };
+    });
+    
+
+/*By Wu Yun Qing*/
+
+ myapp.directive("simpleField",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "js/components/simple_field.html"
+        };
+    });
+    
+    myapp.directive("textareaField",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "js/components/textarea_field.html"
+        };
+    });
+    
+    myapp.directive("imageUpload",function(){
+        return {
+            restrict: "E",
+            scope: {
+                id: '@',
+                label: '@'
+            },
+            templateUrl: "js/components/image_upload.html"
+        };
+    });
+    
+    myapp.directive("eventForm",function(){
+        return {
+            restrict: "E",
+            templateUrl: "js/components/event_form.html"
+        };
+    });
+    
+    myapp.directive("teamForm",function(){
+        return {
+            restrict: "E",
+            templateUrl: "components/team_form.html"
+        };
+    });
