6318597b85db5e6c093a92b82fab87baf05b3e4f
jzhangbs <zhangjingyang33@126.com>
2016-11-05 13:05:45 +0800

    add normal login

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,44 @@
-$(document).ready(function(){
+// $(function () {
+//   $('#myTab li:eq(1) a').tab('show');
+// });
 
+var app = angular.module("indexApp", ["firebase", "ngCookies"]);
 
-    $("#btn_admin").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "admin.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+app.controller("indexCtrl",
+  function($scope, $firebaseArray, $timeout, $cookies, $window) {
 
-    $("#btn_leader").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "team.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+    if (checkLogin($cookies))
+      gotoURL("/profile.html", [{key:"username",value:$cookies.get("username")}], $window);
 
-    $("#btn_member").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "member.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+    initalizeFirebase();
+    var ref = firebase.database().ref("accounts");
+    accounts = $firebaseArray(ref);
 
+    $scope.login = function() {
+      var loginUser;
+      for (var i in accounts) {
+        if ($scope.inputEmail == accounts[i].username && $scope.inputPassword == accounts[i].password)
+          loginUser = $scope.inputEmail;
+      }
+      if (loginUser !== undefined) {
+        $cookies.put("username",loginUser);
+        gotoURL("/profile.html", [{key:"username",value:$cookies.get("username")}], $window);
+      }
+    };
 
-});
+    $scope.rememberMe = false;
+
+    $scope.reg = function() {
+      if ($scope.inputEmailReg !== undefined && $scope.inputEmailReg !== "" &&
+          $scope.inputPasswordReg !== undefined && $scope.inputPasswordReg !== "" &&
+          $scope.inputConfirmPasswordReg !== undefined && $scope.inputConfirmPasswordReg !== "" &&
+          $scope.inputConfirmPasswordReg == $scope.inputPasswordReg) {
+
+        for (var i in accounts)
+          if ($scope.inputEmailReg == accounts[i].username) return;
+        newAcc = {username: $scope.inputEmailReg, password: $scope.inputPasswordReg};
+        accounts.$add(newAcc);
+      }
+    };
+  }
+);
