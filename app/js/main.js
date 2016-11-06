317c5ff0a9f6f827fdd0e307d8d838ee6c8bc93f
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-11-07 01:16:23 +0800

    connected the search event page to firebase, implmented the search event function

--- app/js/main.js
+++ app/js/main.js
@@ -11,14 +11,70 @@ teamapp.config(function($routeProvider) {
 });
 
 
-teamapp.controller('main_ctroller', ['$scope','$firebase','$rootScope', function($scope,$firebase,$rootScope){
+teamapp.controller('main_ctroller', ['$scope','$firebase','$rootScope','$firebaseObject','$firebaseArray', function($scope,$firebase,$rootScope,$firebaseObject,$firebaseArray){
 	$rootScope.user_ref=firebase.database().ref("users");
 	$rootScope.event_ref=firebase.database().ref("events");
 	$rootScope.team_ref=firebase.database().ref("teams");
 
-	console.log($rootScope.user_ref);
+	$rootScope.users=$firebaseArray($rootScope.user_ref);
+	$rootScope.events=$firebaseArray($rootScope.event_ref);
+	$rootScope.teams=$firebaseArray($rootScope.team_ref);
 
+	
 
+	$rootScope.test=function(){
+		$rootScope.loginWithEmail($scope.useremail);
+		console.log($scope.useremail);
+		console.log($rootScope.currentUser);
+
+	}
+
+	$rootScope.addEventTest=function(){
+		exampleEvent.eventName=$scope.eventtitle;
+		$rootScope.addEvent(exampleEvent);
+		console.log($rootScope.events);
+	}
+	$rootScope.loginWithEmail=function(email){
+		
+		for(var i=0;i<$rootScope.users.length;i++){
+			
+			if($rootScope.users[i].email==email){
+				$rootScope.currentUser=$rootScope.users[i];
+				
+			}
+		}
+	}
+	
+
+	var exampleNewUser={
+		eventsManaging:[],
+		email:"abcde@connect.ust.hk",
+		name:"I am new",
+		notifis:[],
+		profilePic:"https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/11811378_670467616420822_8367557776291472237_n.jpg?oh=daf68581e51d412ce96010adf7d77648&oe=588A7872",
+		skills:[],
+		teamsApplying:[],
+		teamsAsLeader:[],
+		teamsAsMember:[]
+
+	}
+
+	var exampleEvent={
+		adminID:"-asdnfkasdfadfad",
+		allTeams:[],
+		description:"A new Event holden by peter",
+		eventName:"My Awsome event",
+		maxSize:10,
+		minSize:5
+	}
+	$rootScope.addUser=function(user){
+		$rootScope.users.$add(user);
+
+	}
+	$rootScope.addEvent=function(event){
+		$rootScope.events.$add(event);
+	}
+	//$rootScope.addUser(exampleNewUser);
 
 }]);
 
