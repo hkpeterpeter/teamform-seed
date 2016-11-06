455a3fbbbd9d63ab4d818d90042c3ab69c19a419
Kit <lungkit211@gmail.com>
2016-11-07 01:16:37 +0800

    明天繼績

--- app/Teaminformation.js
+++ app/Teaminformation.js
@@ -3,12 +3,24 @@ var ti = angular.module("Teaminformation", ["firebase"])
     
 ti.controller('TiCtrl', function($scope) {
 	
-		$scope.loadFunc = function() {
-		var teamID = $.trim( $scope.Team.teamName );		
-		var refPath =  "Team/" + teamID ;
-		retrieveOnceFirebase(firebase, refPath, function(data) {	
-			
-			$scope.$apply(); // force to refresh
+	
+		// Get a database reference to our posts
+		var db = firebase.database();
+		var ref = db.ref("Team");
+
+		// Attach an asynchronous callback to read the data at our posts reference
+		ref.on("teamName", function(snapshot) {
+		  console.log(snapshot.val());
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
 		});
-    }
+
+		// $scope.loadFunc = function() {
+		// var teamID = $.trim( $scope.Team.teamName );		
+		// var refPath =  "Team/" + teamID ;
+		// retrieveOnceFirebase(firebase, refPath, function(data) {	
+			
+		// 	$scope.$apply(); // force to refresh
+		// });
+  //   }
 });
