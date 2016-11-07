0ee65a4164abb8f6d940aeb1511cf8512e56cbc5
a4n0nym0u5 <hades94720@gmail.com>
2016-11-07 15:43:02 +0800

    Updated

--- app/app.route.js
+++ app/app.route.js
@@ -59,19 +59,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
             url: '/search',
             templateUrl: 'search.html',
             controller: 'eventSearch'
-            });
+            })
         
-<<<<<<< HEAD
-<<<<<<< HEAD
       .state('Teaminformation', {
             url: '/Teaminformation',
             templateUrl: 'Teaminformation.html',
             controller: 'tictrl'
             });
-=======
-        
->>>>>>> f410c301e82d83234e31d413e670ee46b3c249e2
-=======
-        
->>>>>>> f410c301e82d83234e31d413e670ee46b3c249e2
+
 });
