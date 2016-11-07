9972a617b5d246be114e5a6f538b0ed5dfc0517e
dht2345 <hdingab@connect.ust.hk>
2016-11-07 23:29:24 +0800

    Request part finished

--- app/js/requestCtrl.js
+++ app/js/requestCtrl.js
@@ -2,7 +2,44 @@
 app.controller("requestCtrl", 
 
 	// Implementation the todoCtrl 
-	function($scope, Auth, $firebaseArray, $firebaseObject) {
+	function($scope, Auth, $firebaseArray, $firebaseObject, Helper) {
 		console.log("request");
+
+		Auth.$onAuthStateChanged(function(authData){
+			$scope.authData = authData;
+			if (authData) {
+				console.log(authData);
+
+				/** */
+				var ref = firebase.database().ref("users/" + authData.uid + "/writable");
+				$scope.myEventList=$firebaseObject(ref);
+
+				var ref2 = firebase.database().ref("events");
+				$scope.allEvents=$firebaseArray(ref2);
+				
+			}
+			else {
+				console.log("signed out");
+				$window.location.href = '/';
+			}
+		});
+
+
+		$scope.withdrawApp = function(eid, tid){
+			Helper.withdrawApplication($scope.authData.uid, eid, tid);
+		}
+
+		$scope.acceptInvi = function(eid, tid){
+			Helper.acceptInvitation($scope.authData.uid, eid, tid);
+		}
+
+		$scope.declineInvi = function(eid, tid){
+			Helper.declineInvitation($scope.authData.uid, eid, tid);
+		}
+
+
+		
 	}
+
+	
 );
