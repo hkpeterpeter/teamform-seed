5f40728ba1eccec505108b8d58199ce806c0e33b
hjiaa <Hejia@HedeMacBook-Air.local>
2016-11-08 02:06:48 +0800

    add "smart add"
    1.add "smart add"
    2.place css into jiahe.css and delete style.css
    3.use spa
    4.rewrite teamleader.js

--- app/JiaHe/teamleader.js
+++ app/JiaHe/teamleader.js
@@ -1,11 +1,23 @@
-var leaderApp = angular.module('leaderApp', []);
+teamapp.controller('teamleader_controll', ['$scope', "$rootScope", function($rootScope, $scope) {
 
-leaderApp.controller('eventCtrl', function($scope) {
-  $scope.event = {
-    name: 'Event Name',
-    maxSize: 10,
-    minSize: 5
-  };
-  $scope.preferredSize = 7;
-  $scope.preferredSkills = ['JavaScript', 'Angularjs'];
-});
+    $scope.event = {
+        name: "Event Name",
+        maxSize: 10,
+        minSize: 5,
+        teams: ["Team1", "Team2"]
+    };
+    $scope.team = {
+        name: "Team name",
+        isPrivate: true,
+        preferredSize: 7,
+        preferredSkills: ["JavaScript", "Angularjs"],
+        newSkill: "",
+        leader: "Leader name",
+        members: ["Member1"],
+        requests: ["Applicant1"]
+    }
+    $scope.addNewSkill = function() {
+        if ($scope.team.newSkill != '' && $scope.team.preferredSkills.indexOf($scope.team.newSkill) == -1)
+        $scope.team.preferredSkills.push($scope.team.newSkill);
+    }
+}]);
