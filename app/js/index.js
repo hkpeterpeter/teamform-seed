2f9c993dd188b0c58878430d601ec273f2cf376e
henryszex42s <henryszex42s@gmail.com>
2016-11-09 00:37:33 +0800

    +Fix Bug +Logout +Store user info to Firebase

--- app/js/index.js
+++ app/js/index.js
@@ -36,36 +36,4 @@ $(document).ready(function(){
 
 
 
-angular.module('teamform-login-app', ['firebase'])
-.controller('LoginCtrl', ['$scope', '$firebaseObject', '$firebaseArray', "$firebaseAuth", function($scope, $firebaseObject, $firebaseArray,$firebaseAuth) {
-         
-    // Call Firebase initialization code defined in site.js
-    initalizeFirebase();
-    $scope.loginwithgoogle = function(){
-    var provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-     // The signed-in user info.
-    var user = result.user;
-    console.log("Logged in as:", result.uid);
-     // ...
-    }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-     var email = error.email;
-     // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    console.error("Authentication failed:", error);
-    // ...
-    });
- };
-
-}]);
-
-
-
 
