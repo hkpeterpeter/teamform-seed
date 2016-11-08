5e6eb7fa5d16c9789ed4b703ef7707a83b7db803
hjiaa <Hejia@HedeMacBook-Air.local>
2016-11-08 23:19:24 +0800

    Change variable names to keep consistent with firebase.

--- app/JiaHe/teamleader.js
+++ app/JiaHe/teamleader.js
@@ -1,32 +1,36 @@
 teamapp.controller('teamleader_controll', ['$scope', "$rootScope", function($rootScope, $scope) {
 
-    $scope.event = {
-        name: "Event Name",
-        maxSize: 10,
-        minSize: 5,
-        teams: ["Team1", "Team2"]
-    };
+    $rootScope.currentEvent = $rootScope.events[0];
+    $scope.event = $rootScope.events[0];
+    //$scope.team = $rootScope.teams[0];
+    $scope.event2 = {
+        eventName: "Event Name",
+        //eventName: $rootScope.events[0].eventName,
+        maxSize: $rootScope.events.length,
+        minSize: $rootScope.teams.length,
+        allTeams: ["Team1", "Team2"]
+    }
     $scope.team = {
         name: "Team name",
         isPrivate: true,
-        preferredSize: 7,
-        preferredSkills: ["JavaScript", "Angularjs"],
+        preferedSize: 7,
+        desiredSkills: ["JavaScript", "Angularjs"],
         newSkill: "",
-        leader: "Leader name",
+        leader: $rootScope.currentUser,
         members: ["Member1"],
         requests: ["Applicant1"]
     }
     $scope.smartPick = {
-        preferredSize: angular.copy($scope.team.preferredSize),
-        preferredSkills: angular.copy($scope.team.preferredSkills),
+        preferedSize: angular.copy($scope.team.preferedSize),
+        desiredSkills: angular.copy($scope.team.desiredSkills),
         newSkill: ""
     }
     $scope.deleteMember = function() {
 
     }
     $scope.addNewSkill = function(scope) {
-        if (scope.newSkill != '' && scope.preferredSkills.indexOf(scope.newSkill) == -1) {
-            scope.preferredSkills.push(scope.newSkill);
+        if (scope.newSkill != '' && scope.desiredSkills.indexOf(scope.newSkill) == -1) {
+            scope.desiredSkills.push(scope.newSkill);
             scope.newSkill = '';
         }
     }
