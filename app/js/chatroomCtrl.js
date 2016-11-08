50ece5cb4bb19e7e2501d989e9cf63962b455f8f
chsunab <chsunab@connect.ust.hk>
2016-11-09 00:00:05 +0800

    Completed the css of chatroom

--- app/js/chatroomCtrl.js
+++ app/js/chatroomCtrl.js
@@ -1,24 +1,57 @@
-var app = angular.module("chatroomApp", ["firebase"]); 
+var app = angular.module("chatroomApp", ["firebase"]);
 
-app.controller("chatroomCtrl", 
+app.controller("chatroomCtrl",
 
-	function($scope, $firebaseArray) {
+    function($scope, $firebaseArray) {
+        var ref = firebase.database().ref("chats");
+        $scope.chatrooms = $firebaseArray(ref);
 
-		$scope.input = {
-			content: "",
-			date: "",
-		}
-	
-		var ref = firebase.database().ref("chatroomApp");
-		$scope.chatrooms = $firebaseArray(ref);
+        //for creating chats
+        //if memberlist contains only one id, that should be group id, else it should be member ids
+        function createChat(memberlist, msgs) {
+            return {
+                "members": memberlist,
+                "msgs": msgs
+            };
+        }
 
-		$scope.addchatroom = function() {
-			
+        function createMsg(content, date) {
+            return {
+                "content": content,
+                "date": date
+            };
+        }
 
-			if ($scope.input.content != "" ) {
-				$scope.input.date = new Date().toString();
-				$scope.chatrooms.$add($scope.input);
-			}
-		}
-	}
+        function hasChatRecord(memberlist) {
+            //TODO check if the group is already exist, and check if there are old chat records
+            //if yes, return the index of the array, else return -1
+
+            return -1;
+        }
+
+        function addChatRecord() {
+            //transform the memberlist string into array
+            $scope.input.members = $scope.input.members.split(" ").join("").split(",");
+
+            var index = hasChatRecord($scope.input.members);
+
+            if (index >= 0) {
+                var msg = createMsg($scope.tempContent, new Date().toString);
+                $firebaseArray("chats/" + index + "/msgs").$add(msg);
+            } else
+                $scope.chatrooms.$add(createChat($scope.input.members,
+                    [createMsg($scope.tempContent, new Date().toString)]));
+        }
+
+        //initialization, create empty chat
+        $scope.input = createChat([], "");
+        $scope.tempContent = "";
+
+        $scope.addNewMessage = function() {
+            if ($scope.input.to != "" && $scope.input.content != "") {
+                addChatRecord();
+            }
+        }
+
+    }
 );
