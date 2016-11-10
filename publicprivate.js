a57ef90c86427f7af713d43f44972a309ab983d5
a4n0nym0u5 <hades94720@gmail.com>
2016-11-10 18:32:23 +0800

    created public/private function

---
+++ publicprivate.js
@@ -0,0 +1,30 @@
+
+app.controller("openness",["$scope","$firebaseArray", "Auth","$rootScope",
+               
+  function($scope, $firebaseArray,Auth,$rootscope){
+                
+                var ref=firebase.database().ref("events");
+                $scope.event = $firebaseArray(ref);
+
+$scope.idd=firebase.auth().currentUser.uid;
+        $scope.toprivate = function(eventobj,teamid) {
+
+          var path = "events" + "/" + eventobj.$id +"/Team/" +teamid;
+          console.log(path);
+         var itemRef =firebase.database().ref(path);
+         itemRef.update({openness : 'private' });
+         console.log('done');
+            }
+ $scope.topublic = function(eventobj,teamid) {
+
+          var path = "events" + "/" + eventobj.$id +"/Team/" +teamid;
+          console.log(path);
+         var itemRef =firebase.database().ref(path);
+         itemRef.update({openness : 'public' });
+         console.log('done');
+            }
+    
+               }
+               
+]);
+               
