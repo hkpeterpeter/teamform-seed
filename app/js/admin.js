a792382abebb915ca45d70c4b3ec956c8769ed5b
henryszex42s <henryszex42s@gmail.com>
2016-11-12 05:01:16 +0800

    Add Event Page and Bug Fix

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-	
-	$('#admin_page_controller').hide();
-	$('#text_event_name').text("Error: Invalid event name ");
-	var eventName = getURLParameter("q");
-	if (eventName != null && eventName !== '' ) {
-		$('#text_event_name').text("Event name: " + eventName);
-		
-	}
+
+    $("#btn_event").click(function(){
+    	var val = $('#input_text').val();
+    	if ( val !== '' ) {
+    		var url = "event.html?q=" + val;
+			var event_name = $('#input_text').val();
+    		window.location.href = url ;
+    		return false;
+    	}
+    });
 
 });
+
+
+
+
+
 
 angular.module('teamform-admin-app', ['firebase'])
 .controller('AdminCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
@@ -20,7 +27,7 @@ angular.module('teamform-admin-app', ['firebase'])
 			
 	// Call Firebase initialization code defined in site.js
 	initalizeFirebase();
-	
+	$scope.input_text;
 	var refPath, ref, eventName;
 	
 	eventName = getURLParameter("q");
@@ -50,46 +57,6 @@ angular.module('teamform-admin-app', ['firebase'])
 		});
 		
 	
-	refPath = eventName + "/team";	
-	$scope.team = [];
-	$scope.team = $firebaseArray(firebase.database().ref(refPath));
-	
-	
-	refPath = eventName + "/member";
-	$scope.member = [];
-	$scope.member = $firebaseArray(firebase.database().ref(refPath));
-	
-	
-
-	$scope.changeMinTeamSize = function(delta) {
-		var newVal = $scope.param.minTeamSize + delta;
-		if (newVal >=1 && newVal <= $scope.param.maxTeamSize ) {
-			$scope.param.minTeamSize = newVal;
-		} 
-		
-		$scope.param.$save();
-
-		
-	}
-
-	$scope.changeMaxTeamSize = function(delta) {
-		var newVal = $scope.param.maxTeamSize + delta;
-		if (newVal >=1 && newVal >= $scope.param.minTeamSize ) {
-			$scope.param.maxTeamSize = newVal;
-		} 
-		
-		$scope.param.$save();
-		
-		
-	}
-
-	$scope.saveFunc = function() {
-
-		$scope.param.$save();
-		
-		// Finally, go back to the front-end
-		window.location.href= "index.html";
-	}
 	
 		
 }]);
