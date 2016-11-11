1dc3d879f453f311493853d7befd1a9c37882ec1
RonaldShum <khshumab@connect.ust.hk>
2016-11-11 13:28:25 +0800

    leader.js (#40)
    * test_create_team
    
    * leader.js_template
    
    * modified index.js

---
+++ app/js/leader.js
@@ -0,0 +1,66 @@
+angular.module('leader-app', ['firebase'])
+.controller('LeaderCtrl', ['$scope', '$firebaseObject', '$firebaseArray','$window', function($scope, $firebaseObject, $firebaseArray,$window) {
+
+
+    //sth that should be include for navbar
+	$scope.loggedIn = true;
+	// Call Firebase initialization code defined in site.js
+	initalizeFirebase();	
+
+    //eventid & teamid from url
+    $scope.eventid = getURLParameter("eventid");
+    $scope.teamid = getURLParameter("teamid");
+
+    //create team function
+    // $scope.createTeam = function(){
+    //     console.log('creating team');
+    //     var ref = firebase.database().ref("events/"+$scope.eventid);
+    //     console.log(ref);
+    //     var teamkey = ref.push().key;
+    //     console.log(teamkey);
+    //     var event = $firebaseObject(ref);
+    //     event.$loaded()
+    //         .then(function(data){
+    //             console.log(data);
+    //             var newteamRef = firebase.database().ref('events/'+$scope.eventid+'/'+teamkey);
+    //             var teamobject = $firebaseObject(newteamRef);
+    //             teamobject.teamName = "test name";
+    //             teamobject.$save();
+    //         });
+    // }
+
+    //$scope.users is an array of users in firebase
+    var usersRef = firebase.database().ref('users');
+    $scope.users = $firebaseArray(usersRef);
+	
+    
+
+	//logout function
+	$scope.logout = function(){
+		firebase.auth().signOut();
+	}
+
+//monitor if the user is logged in or not
+	firebase.auth().onAuthStateChanged(user => {
+		if(user){
+            console.log('leader.js')
+			console.log('logged in');
+            var database = firebase.database();
+            var usersRef = database.ref('users/'+user.uid);
+            var currentUserData = $firebaseObject(usersRef);
+            currentUserData.$loaded()
+                .then(function(data){
+                    $scope.username = currentUserData.name;
+                })
+                .catch(function(error){
+                    console.error("Error: "+error);
+                });
+            $scope.loggedIn = true;
+			$scope.uid = user.uid;
+        }else{
+			console.log('not log in');
+            $window.location.href = '/index.html';
+		}
+	})
+}]);
+
