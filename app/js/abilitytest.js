cb762178da16d7185a49fa4917b0458b32f736f9
ykcsteven <kcyauac@connect.ust.hk>
2016-11-11 15:45:17 +0800

    add the function of submit button

--- app/js/abilitytest.js
+++ app/js/abilitytest.js
@@ -13,7 +13,35 @@ $(document).ready(function(){
 angular.module('ability-test-app', ['firebase'])
 .controller('AbilityTestCtrl', ['$scope', '$firebaseObject', '$firebaseArray',
 	function($scope, $firebaseObject, $firebaseArray) {
-
-    $scope.quiz = [];
+	
+	firebase.auth().onAuthStateChanged(function(firebaseUser) {
+		if(firebaseUser) {
+			var user = firebase.auth().currentUser;
+	        	$scope.uid = user.uid;
+		}
+	});
+	
+	$scope.quiz = [];
   	$scope.quiz = $firebaseArray(firebase.database().ref("quiz/java"));
+		
+	$scope.param = {
+		"answer" : []
+	};
+		
+	$scope.addanswer = function(option){
+		$scope.param.answer.push(option);
+	};
+
+	$scope.submitFunc = function() {
+		var userID = $.trim($scope.uid);
+		var newData = {
+			'answers': $scope.param.answers
+		};
+		var refPath = getURLParameter("q") +"/member/" + userID;
+		var ref = firebase.database().ref(refPath);
+		ref.update({answer: $scope.param.answer})
+		window.location.href = "index.html";
+	};
+		
+	
 }]);
