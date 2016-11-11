786e78694205d305df1c2249103802a3b17a353c
kayiuc <danielkayiu@hotmail.com>
2016-11-11 17:42:11 +0800

    teaminfo again

---
+++ app/js/teaminformation.js
@@ -0,0 +1,11 @@
+app.controller("teaminfo",["$scope","$firebaseArray", "Auth",
+               
+               function($scope, $firebaseArray,Auth,){
+                
+                var ref=firebase.database().ref("teams");
+                $scope.teams = $firebaseArray(ref);	
+               }
+          
+               
+]);
+    
