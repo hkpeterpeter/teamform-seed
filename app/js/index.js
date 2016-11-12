3a386b4ee8484c389f39c429bd5a962556728166
WongYat2000 <ywongac@connect.ust.hk>
2016-11-12 18:27:37 +0800

    Angularize index page

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,41 @@
-$(document).ready(function(){
+angular.module('teamform-index-app', ['firebase'])
+.controller('IndexCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function ($scope, $firebaseObject, $firebaseArray) {
+
+    // TODO: implementation of MemberCtrl
 
 
-    $("#btn_admin").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "admin.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+    // Call Firebase initialization code defined in site.js
+    initalizeFirebase();
 
-    $("#btn_leader").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "team.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
-
-    $("#btn_member").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "member.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+    $scope.eventID = "";
 
 
-});
+
+    $scope.btn_admin = function() {
+        var val = $scope.eventID;
+        if (val !== '') {
+            var url = "admin.html?q=" + val;
+            window.location.href = url;
+            //return false;
+        }
+    }
+
+    $scope.btn_leader = function() {
+        var val = $scope.eventID;
+        if (val !== '') {
+            var url = "team.html?q=" + val;
+            window.location.href = url;
+            //return false;
+        }
+    }
+
+    $scope.btn_member = function() {
+        var val = $scope.eventID;
+        if (val !== '') {
+            var url = "member.html?q=" + val;
+            window.location.href = url;
+            //return false;
+        }
+    }
+
+}]);
