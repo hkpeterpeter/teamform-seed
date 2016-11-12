77527b1cf9646eff181fc8643685e79f4254fce9
KUEN\cckhhu6495 <cckhhu6495@gmail.com>
2016-11-13 05:00:45 +0800

    make the event and team into two seperated value

--- app/randomTeam.js
+++ app/randomTeam.js
@@ -1,14 +1,23 @@
 app.controller("random_Team",["$scope","$firebaseArray", "Auth","$rootScope",
                
-               function($scope, $firebaseArray,Auth,$rootscope){
-                
-                var ref=firebase.database().ref("events");
-                $scope.event = $firebaseArray(ref);
-
-                
-                
-               }
-               
-               
+	function($scope, $firebaseArray,Auth,$rootscope){	
+		var ref=firebase.database().ref("events");
+		$scope.event = $firebaseArray(ref); 
+		
+		
+		//eventID.$bindTo($scope, "selectedName");
+		$scope.setEvent =function(ID){
+			var eventID=ID;
+			var path= "events" + "/" + ID + "/" + "Team" ;
+			var eventRef= firebase.database().ref(path);
+			$scope.teamOfChosenEvent= $firebaseArray(eventRef);			
+		}
+		
+		//var eventId=$("#selectedName").val();
+		//var path= "events" + "/" + eventID + "/" + "Team" ;
+		//var eventRef= firebase.database().ref(path);
+		//$scope.teamOfChosenEvent= $firebaseArray(eventRef);
+		//teamOfChosenEvent.shuffle(Random random);
+		
+	}	
 ]);
-               
