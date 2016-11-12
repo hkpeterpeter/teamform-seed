70532e8769c63c68d6b9a8ed6c4d02073e145062
mnlauaa <mnlauaa@connect.ust.hk>
2016-11-12 17:51:55 +0800

    Tasting

---
+++ app/login/Question.js
@@ -0,0 +1,38 @@
+app.controller("Question", ["$scope", "$firebaseArray","$firebaseObject",
+    function($scope, $firebaseArray, $firebaseObject) {
+    var ref = firebase.database().ref('Question/Q1');
+
+    $scope.q = {
+            question: "",
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+            ans: ""
+    }
+
+     var obj = $firebaseObject(ref);
+
+     // to take an action after the data loads, use the $loaded() promise
+     obj.$loaded().then(function() {
+        $scope.q.question = obj.q1;
+        $scope.q.a = obj.a;
+        $scope.q.b = obj.b;
+        $scope.q.c = obj.c;
+        $scope.q.d = obj.d;
+        $scope.q.ans = obj.correct;
+    });
+
+
+    $scope.sumitans = function() {
+    		if($scope.Qinput == $scope.q.ans){
+    			console.log("correct");
+    		}
+    		else{
+    			console.log("XXX");
+    		}
+        
+    }
+
+  }
+]);
