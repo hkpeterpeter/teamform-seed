bd1860a172e69fe86bb57a80408f20896a74043f
chsunab <chsunab@connect.ust.hk>
2016-11-13 01:30:08 +0800

    chatroom is ready

--- app/js/chatroomCtrl.js
+++ app/js/chatroomCtrl.js
@@ -16,23 +16,31 @@ app.controller("chatroomCtrl",
             }]
         }
 
-        $scope.hasChatRecord = function () {
-            //TODO check if the group is already exist, and check if there are old chat records
-            //if yes, return the index of the array, else return -1
-            // for(var i=0;i<;i++){
-
-            // }
+        $scope.hasChatRecord = function (currentMemberList) {
+            
+            var checking = true;
+       // for(var t=0;t<$scope.chatrooms.length;t++){
+             for(var i=0;i<$scope.chatrooms[0].members.length;i++){
+                 for(var j=0;j<currentMemberList.length;j++){
+                    if($scope.chatrooms[0].members[i]==currentMemberList[j]){
+                        break;
+                    }
+                    if($scope.chatrooms[0].members[i]!=currentMemberList[j] && j==(currentMemberList.length-1)){
+                        checking=false;
+                        return -1;
+                    }
+                }
+                if(i==($scope.chatrooms[0].members.length-1)){
+                    return 0;}
+            }
+        //}
             return -1;
         }
 
         $scope.addChatRecord = function () {
             //$scope.input.members = $scope.input.members.split(" ").join("").split(",");
-
-            var index = $scope.hasChatRecord();
-
-            //for testing, dummy data
-            index = 1;  //assume the records exist, and it is located at index 1
             var currentMemberList = ["g0001"];
+            var index = $scope.hasChatRecord(currentMemberList);
 
             if (index >= 0) {
                 $scope.chatrooms.$ref().child(index).child("msgs").child($scope.chatrooms[index].msgs.length).set({
@@ -42,7 +50,7 @@ app.controller("chatroomCtrl",
             }
             else {
                 $scope.chatrooms.$ref().child($scope.chatrooms.length).set({
-                    "members": ["g111", "g111", "g111"],
+                    "members": currentMemberList,
                     "msgs": [{ "content": $scope.tempcontent, "date": new Date().toString() }]
                 });
             }
