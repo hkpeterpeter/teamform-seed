9687ca4c0a88b9266ef0224f505ac91afad3b798
henryszex42s <henryszex42s@gmail.com>
2016-11-13 22:33:36 +0800

    no message

--- app/assets/js/create_event.js
+++ app/assets/js/create_event.js
@@ -40,32 +40,6 @@ angular.module('teamform-create_event-app', ['firebase'])
 	// Link and sync a firebase object
 	
 	$scope.param = $firebaseObject(ref);
-
-
-
-	$scope.param.$loaded()
-		.then( function(data) {
-			// Fill in some initial values when the DB entry doesn't exist			
-			if(typeof $scope.param.TeamSize == "undefined"){				
-				$scope.param.TeamSize = 3;
-			}
-
-			if(typeof $scope.param.MaxTeam == "undefined"){				
-				$scope.param.MaxTeam = 12;
-			}
-			// Enable the UI when the data is successfully loaded and synchornized
-			$('#event_page_controller').show(); 
-
-			var user = firebase.auth().currentUser;
-			$scope.param.EventAdmin = user.displayName;
-			$scope.param.EventName = eventName;
-			$scope.param.No_of_Team = 0;		
-		}) 
-		.catch(function(error) {
-			// Database connection error handling...
-			//console.error("Error:", error);
-		});
-		
 	
 	refPath = "/event/"+ eventName + "/team";	
 	$scope.team = [];
@@ -75,30 +49,6 @@ angular.module('teamform-create_event-app', ['firebase'])
 	refPath = "/event/"+ eventName + "/team/member";
 	$scope.member = [];
 	$scope.member = $firebaseArray(firebase.database().ref(refPath));
-	
-	
 
-	$scope.changeTeamSize = function(delta) {
-		var newVal = delta;
-		$scope.param.TeamSize = newVal;
-		$scope.param.$save();
-	}
-
-	$scope.changeMaxTeams = function(delta) {
-
-		var newVal = $scope.param.MaxTeam + delta;
-		if (newVal < 10 && newVal > 0 ) {
-			$scope.param.MaxTeam = newVal;
-		} 
-		$scope.param.$save();
-	}
-
-
-	$scope.saveFunc = function() {
-		$scope.param.$save();
-		
-		// Finally, go back to the front-end
-		window.location.href= "index.html";
-	}
  
 }]);
--- app/assets/js/manage_event.js
+++ app/assets/js/manage_event.js
@@ -88,7 +88,7 @@ angular.module('teamform-event-app', ['firebase'])
 		$scope.param.$save();
 		
 		// Finally, go back to the front-end
-		window.location.href= "index.html";
+		window.location.href= "event.html?q=" +eventName;
 	}
 	
 		
