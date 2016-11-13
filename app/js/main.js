8c8d83a284a925acf0602899587e7b2a0166aea6
Vishal Bollu <vbollu@uwaterloo.ca>
2016-11-13 23:16:52 +0800

    adding ui router, moving base html outside of the app folder so that it has access to node_modules and adding a base route and creating a partial

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 (function (angular) {
     'use strict';
 
-    angular.module("teamform", []
+    angular.module("teamform", ['ui.router']
     ).run(['$rootScope', function ($rootScope) {
+    }]).config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider){
+
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider.state('landing', {
+                url: '/',
+                templateUrl: 'app/partials/landing.html'
+            });
     }]);
 }(angular));
