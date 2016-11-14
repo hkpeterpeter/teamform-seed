d2ad72ad8e474addeffc81bfa6223961bf12f0db
WongYat2000 <ywongac@connect.ust.hk>
2016-11-14 15:19:32 +0800

    Added a signin page for admin. Facebook account cannot go to admin page through button. (still able by editing url)

--- app/js/index.js
+++ app/js/index.js
@@ -7,15 +7,25 @@ angular.module('teamform-index-app', ['firebase'])
     // Call Firebase initialization code defined in site.js
     initalizeFirebase();
     $scope.eventID = "";
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            $scope.$apply($scope.logined = true);
+            $scope.$apply($scope.username = user.displayName);
+            if ($scope.username == null) { $scope.$apply($scope.fb = false); $scope.$apply($scope.username = user.email); } else { $scope.$apply($scope.fb = true); };
+        } else {
+            // No user is signed in.
+        }
+    });
     $scope.btn_fb = function () {
         var provider = new firebase.auth.FacebookAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function (result) {
                 // This gives you a Facebook Access Token. You can use it to access the Facebook API.
                 var token = result.credential.accessToken;
                 // ...
+                $scope.$apply($scope.username = result.user.displayName);
                 $scope.$apply($scope.logined = true);
             // The signed-in user info.
-                $scope.$apply($scope.currentUser = result.user);
                 
         }).catch(function (error) {
             // Handle Errors here.
@@ -36,6 +46,10 @@ angular.module('teamform-index-app', ['firebase'])
             window.location.href = url;
             //return false;
         }
+    }
+
+    $scope.btn_email = function () {
+       window.location.href = "signin.html";
     }
 
     $scope.btn_leader = function() {
---
+++ app/js/signin.js
@@ -0,0 +1,21 @@
+﻿angular.module('teamform-signin-app', ['firebase'])
+.controller('SigninCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function ($scope, $firebaseObject, $firebaseArray) {
+
+    // TODO: implementation of MemberCtrl
+
+
+    // Call Firebase initialization code defined in site.js
+    initalizeFirebase();
+    $scope.btn_signin = function () {
+        firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            window.location.href = "error.html";
+        });
+
+        window.location.href = "index.html";
+    }
+
+}]);
