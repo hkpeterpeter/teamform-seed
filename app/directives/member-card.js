230fbdc0e6c5077537e0ae71baa714f9dfd13dd0
Tonia Tong <vbollu@propelme.ca>
2016-11-13 17:50:04 -0800

    Fix #23
    Creating the member card directive for the entire app.

--- app/directives/member-card.js
+++ app/directives/member-card.js
@@ -1,19 +1,17 @@
 (function (angular) {
     'use strict';
 
-    console.log("running");
-
     /*
      * A template for member cards to be used throughout the website.
      */
-    angular.module('teamform-member-app', []).directive('memberCard', function () {
+    angular.module('teamform').directive('memberCard', function () {
         return {
             restrict: 'E',
             replace: true,
             scope: {
                 data: '=data'
             },
-            templateUrl: 'directives/templates/member-card.html'
+            templateUrl: 'app/directives/templates/member-card.html'
         };
     });
 }(angular));
