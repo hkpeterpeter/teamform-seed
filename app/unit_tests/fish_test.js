40eb35e459bb89a4e65e9a0680851ab439b7e9f1
YuWanZ <zzhaoah@ust.hk>
2016-11-15 02:46:12 +0800

    update test

--- app/unit_tests/fish_test.js
+++ app/unit_tests/fish_test.js
@@ -1,14 +1,18 @@
 describe('fishCtrl',function(){
 	beforeEach(module('teamapp','firebase'));
-	var $scope, $firebaseArray, $firebaseObject, createController, firebaseSync;
+	var $scope, $firebaseArray, $firebaseObject, createController, $q, defered;
 	initFirebase();
 
-	beforeEach(inject(function($rootScope, $controller, _$rootScope_, _$firebaseArray_, _$firebaseObject_){
+	beforeEach(inject(function($rootScope, $controller, _$rootScope_, _$firebaseArray_, _$firebaseObject_, _$q_){
 		$firebaseArray = _$firebaseArray_;
 		$firebaseObject = _$firebaseObject_;
-		//$controller = _$controller_;
+
+		$q = _$q_;
 		$rootScope = _$rootScope_;
 		$scope = $rootScope.$new();
+		deferred = _$q_.defer();
+
+	//spyOn($scope.allData.$loaded(), 'teamapp').and.returnValue(deferred.promise);
 
 		createController = function() {
 			return $controller('fishCtrl', {
@@ -40,6 +44,12 @@ describe('fishCtrl',function(){
 		$scope.clickCount=1;
 		$scope.initShowBody("a");
 		expect($scope.clickCount).toBe(1);
+	});
+	it('allData promise', function(){
+		
+		var controller = createController();
+
+
 	});
 
 
