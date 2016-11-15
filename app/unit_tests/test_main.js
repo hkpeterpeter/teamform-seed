a90fad02555e0db7390c87c2fe79792988c22674
Vishal Bollu <vbollu@uwaterloo.ca>
2016-11-15 16:24:21 +0800

    main.js test

---
+++ app/unit_tests/test_main.js
@@ -0,0 +1,43 @@
+describe('Test main.js', function () {
+
+    var firebase = {
+        database: function () {
+            return {
+                ref: function (item1) {
+                    return {
+                        once: function (item2) {
+                            return {
+                                then: function (func) {
+                                    func();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    var $rootScope, $state, $injector;
+    beforeEach(function () {
+
+        module('teamform');
+
+        inject(function (_$rootScope_, _$state_, _$injector_) {
+            $rootScope = _$rootScope_;
+            $state = _$state_;
+            $injector = _$injector_;
+        })
+    });
+
+
+    it('value within 1 to 3', function () {
+        var obj = {
+            callback: function () {
+            }
+        };
+        spyOn(obj, "callback");
+        $rootScope.retrieveOnceFirebase(firebase, "", obj.callback);
+        expect(obj.callback).toHaveBeenCalled();
+    });
+});
