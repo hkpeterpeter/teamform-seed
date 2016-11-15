b8ee209c4a18e0e5e60327d7ef6aaec96170119b
Fish <fishyu624@gmail.com>
2016-11-15 19:14:06 +0800

    Delete dummy text

--- karma.conf.js
+++ karma.conf.js
@@ -1,59 +1,41 @@
 //jshint strict: false
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    basePath: './app',
-	frameworks: ['jasmine'],
-    files: [
-	    'lib/jquery.min.js',
-      'lib/angular.min.js',
-      'lib/angular-route.min.js',
-      'lib/angular-mocks.js',
-<<<<<<< HEAD
-	    'lib/firebase.js',
-	    'lib/angularfire.min.js',	  
-	    'app.signup_and_login.js',
-	    'app.js',
-      'unit_tests/test_login.js',
-			'randomEvent.js'	  
-    ],
-	exclude: [
-	],
-	preprocessors: {	 	
-		 'app.signup_and_login.js' : ['coverage'],
-		 'randomEvent.js' : ['coverage']
-=======
-	  'lib/firebase.js',
-	  'lib/angularfire.min.js',
-    'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js',
-	  'app.js',
-    'app.route.js',
-	  'app.signup_and_login.js',
+        basePath: './app',
+        frameworks: ['jasmine'],
+        files: [
+            'lib/jquery.min.js',
+            'lib/angular.min.js',
+            'lib/angular-route.min.js',
+            'lib/angular-mocks.js',
+            'lib/firebase.js',
+            'lib/angularfire.min.js',
+            'app.signup_and_login.js',
+            'app.js',
+            'unit_tests/test_login.js',
+            'randomEvent.js'
+        ],
+        exclude: [],
+        preprocessors: {
+            'app.signup_and_login.js': ['coverage'],
+            'randomEvent.js': ['coverage']
+        },
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/',
+            subdir: '.'
+        },
+        port: 8080,
+        colors: true,
+        browsers: ['Chrome'],
+        singleRun: true,
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-coverage'
+        ]
 
-      'unit_tests/test_login.js'	  
-    ],
-	exclude: [
-	],
-	preprocessors: {
-    'app.js' : ['coverage'],
-		 'app.signup_and_login.js' : ['coverage']
->>>>>>> 52b51433d3f918c09bbb373d17303243aefbcf3e
-	},
-	reporters: ['progress', 'coverage'],
-	coverageReporter: {
-			type: 'html',
-			dir: 'coverage/',
-			subdir: '.'
-	},
-	port: 8080,
-	colors: true,
-    browsers: ['Chrome'],
-	singleRun: true,
-    plugins: [
-      'karma-chrome-launcher',      
-      'karma-jasmine',
-	    'karma-coverage'
-    ]    
-
-  });
+    });
 };
