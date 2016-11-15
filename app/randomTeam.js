ed760bf884a1124ecac90fbbcf5474aa811b2512
KUEN\cckhhu6495 <cckhhu6495@gmail.com>
2016-11-16 02:49:28 +0800

    fix small bug

--- app/randomTeam.js
+++ app/randomTeam.js
@@ -3,25 +3,33 @@ app.controller("random_Team",["$scope","$firebaseArray", "Auth","$rootScope",
 	function($scope, $firebaseArray,Auth,$rootscope){	
 		var ref=firebase.database().ref("events");
 		$scope.event = $firebaseArray(ref); 
-		
-		
-		//eventID.$bindTo($scope, "selectedName");
+
 		$scope.setEvent =function(ID){
 			var eventID=ID;
 			var path= "events" + "/" + ID + "/" + "Team" ;
 			var eventRef= firebase.database().ref(path);
-			$scope.teamOfChosenEvent= $firebaseArray(eventRef);			
+			return $scope.teamOfChosenEvent= $firebaseArray(eventRef);
 		}
+		
 		$scope.random = function(){
 			return 0.5 - Math.random();
 		};
-		//shuffle(teamOfChosenEvent);
 		
-		//var eventId=$("#selectedName").val();
-		//var path= "events" + "/" + eventID + "/" + "Team" ;
-		//var eventRef= firebase.database().ref(path);
-		//$scope.teamOfChosenEvent= $firebaseArray(eventRef);
-		//teamOfChosenEvent.shuffle(Random random);
+		
+		/*var shuffleArray = function(array) {
+			var m = array.length, t, i;
+			// While there remain elements to shuffle
+			while (m) {
+				// Pick a remaining element…
+				i = Math.floor(Math.random() * m--);
+					// And swap it with the current element.
+				t = array[m];
+				array[m] = array[i];
+				array[i] = t;
+			}
+			return array;
+		}*/
+		//shuffleArray($scope.teamOfChosenEvent);
 		
 	}	
 ]);
