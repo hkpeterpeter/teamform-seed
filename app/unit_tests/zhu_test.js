0c975658a4ea0300ae4005788cf5d3967de5b7ab
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-11-15 19:54:51 +0800

    hh

--- app/unit_tests/zhu_test.js
+++ app/unit_tests/zhu_test.js
@@ -1,16 +1,37 @@
+initFirebase();
 describe('Test site.js', function() {
 	
-   //
-   // Example: A test case of getRandomIntInclusive
-   //
-   describe('Test', function() {
+		beforeEach(module('teamapp'));
+		var $controller
+		var $rootScope;
+		var $compile;
+		var $httpBackend;
+		beforeEach(inject(function(_$controller_,_$rootScope_,_$compile_,$injector){
+			$controller=_$controller_;
+			$rootScope=_$rootScope_;
+			$compile=_$compile_;
 
-	  it('value within 1 to 3', function() {
-	  	var value = 2;
-	  	expect( value>=1 && value <= 3 ).toEqual(true);
-	  });
 
-   });
+			 $httpBackend = $injector.get('$httpBackend');
+  			$httpBackend.whenGET('zhuxinyu/js/components/imageBoard/imageBoard.html').respond(200, '{{content}}');
+		}));
+
+	  
+
+	describe('Test Directives', function() {
+		 it('image-board',function(){
+			
+
+			var element=$compile("<image-board content='ABCD Hello'></image-board>")($rootScope);
+		  	$rootScope.$digest();
+			
+			
+
+		 	expect(element.html()).toContain("ABCD Hello");
+		 });
+
+
+	});
 
 
 });
