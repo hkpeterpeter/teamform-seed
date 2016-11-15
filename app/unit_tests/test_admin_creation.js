cad1de6758da7b1670b1bfc82155aa6ce9ce864f
mallika <mtrivedi@uwaterloo.ca>
2016-11-16 03:36:38 +0800

    Finished testing admin creation controller. 100% code coverage

--- app/unit_tests/test_admin_creation.js
+++ app/unit_tests/test_admin_creation.js
@@ -24,4 +24,52 @@ describe('Test admin.js', function () {
         expect($rootScope.param.maxTeamSize).toBeGreaterThanOrEqual(1)
     });
 
+    it('test change min size method', function () {
+        var controller = createController();
+        // increase min size
+        $rootScope.param.minTeamSize = 2;
+        $rootScope.changeMinTeamSize(3)
+        expect($rootScope.param.minTeamSize).toBe(5);
+
+        // increase min size beyond maxTeamSize
+        $rootScope.param.maxTeamSize = 4;
+        $rootScope.param.minTeamSize = 2;
+        $rootScope.changeMinTeamSize(3)
+        expect($rootScope.param.minTeamSize).toBe(2);
+
+        // decrease min size (invalid case)
+        $rootScope.param.minTeamSize = 2;
+        $rootScope.changeMinTeamSize(-1)
+        expect($rootScope.param.minTeamSize).toBe(1);
+
+        // decrease min size beyond 1 (invalid case)
+        $rootScope.param.minTeamSize = 1;
+        $rootScope.changeMinTeamSize(-1)
+        expect($rootScope.param.minTeamSize).toBe(1);
+    });
+
+    it('test change max size method', function () {
+        var controller = createController();
+        // increase max size
+        $rootScope.param.maxTeamSize = 2;
+        $rootScope.changeMaxTeamSize(3)
+        expect($rootScope.param.maxTeamSize).toBe(5);
+
+        // decrease max size
+        $rootScope.param.maxTeamSize = 2;
+        $rootScope.changeMaxTeamSize(-1)
+        expect($rootScope.param.maxTeamSize).toBe(1);
+
+        // decrease max size beyond 1 (invalid case)
+        $rootScope.param.maxTeamSize = 1;
+        $rootScope.changeMaxTeamSize(-1)
+        expect($rootScope.param.maxTeamSize).toBe(1);
+
+        // decrease max size beyond teamMinSize (invalid case)
+        $rootScope.param.minTeamSize = 5;
+        $rootScope.param.maxTeamSize = 6;
+        $rootScope.changeMaxTeamSize(-3)
+        expect($rootScope.param.maxTeamSize).toBe(6);
+    });
+
 });
