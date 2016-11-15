cfb0a190483b803e1010b95c609a6ddb26f36b20
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-11-15 17:00:38 +0800

    Test

---
+++ app/unit_tests/zhu_test.js
@@ -0,0 +1,16 @@
+describe('Test site.js', function() {
+	
+   //
+   // Example: A test case of getRandomIntInclusive
+   //
+   describe('Test', function() {
+
+	  it('value within 1 to 3', function() {
+	  	var value = 2;
+	  	expect( value>=1 && value <= 3 ).toEqual(true);
+	  });
+
+   });
+
+
+});
