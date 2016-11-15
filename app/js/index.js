f3e084cf6c4347e586a85bb677a61cbe38b312f2
chun tong yeung <chun tong yeung>
2016-11-16 02:05:09 +0800

    added function to retrieve team information for building the multi-level menu

--- app/js/index.js
+++ app/js/index.js
@@ -30,8 +30,62 @@ $(document).ready(function(){
 
 });
 
-angular.module('teamform-index-app', ['firebase', 'login', 'search'])
+angular.module('teamform-index-app', ['login','search','firebase'])
 .controller('indexCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+
+$scope.click_team = function(){
+  $scope.pFilter = "team_info";
+  $scope.$apply();
+
+}
+  initalizeFirebase();
+  var id = "";
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    user.providerData.forEach(function (profile) {
+    id = firebase.auth().currentUser.uid;
+
+  });
+  } else {
+    // No user is signed in.
+  }
+  });
+
+  var path = "";
+
+  path = "events/";
+  $scope.team = [];
+  $scope.member = [];
+
+  firebase.database().ref(path).orderByChild("eventName").on("child_added", function(data) {
+
+    path = "events/" + data.key + "/teams/";
+    var event = data.key;
+    firebase.database().ref(path).orderByChild("teams").on("child_added", function(data) {
+    path = "events/" + event + "/teams/" + data.key + "/members/" ;
+    var team = data.key;
+      firebase.database().ref(path).orderByChild("members").on("child_added", function(data) {
+          if(id == data.val().uid)
+          {
+            if(data.val().role == "leader")
+            {
+              $scope.team.push({"event": event, "team" : team, "link" : "event.html?e=" + event + "&t=" + team });
+            }
+            else if(data.val().role == "member"){
+              $scope.member.push({"event": event, "team" : team, "link" : "event.html?e=" + event + "&t=" + team });
+            }
+            $scope.$apply();
+
+          }
+      });
+
+
+    });
+
+
+  });
+
 
 
 
