db40f518fba2d8029c490c3ab740d267568c49e1
TsuiHinCHi <hctsuiaa@connect.ust.hk>
2016-11-16 14:31:22 +0800

    random grouping

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var app = angular.module('plunker', ['ngTagsInput']);
+var app = angular.module('app', []);
 
-app.controller('MainCtrl', function($scope, $http) {
-               $scope.tags = [
-                              { text: 'Tag1' },
-                              { text: 'Tag2' },
-                              { text: 'Tag3' }
-                              ];
+app.filter('randomSample', function() {
+           return function(array, length) {
+           var copy = angular.copy(array);
+           var sample = [];
+           while(sample.length < length) {
+           var randomIndex = Math.floor(Math.random() * (copy.length));
+           sample.push(copy[randomIndex]);
+           }
+           return sample;
+           };
+           });
+
+app.controller('Ctrl', function($scope, $interval) {
+               $scope.array = [];
+               for(var i = 0; i < 10; i++) {
+               $scope.array.push(i);
+               }
+               
+               $interval(function() {
+                         
+                         }, 1000);
                });
