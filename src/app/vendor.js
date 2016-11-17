99d40d5aa73c08846238a39b3f9a718812a7685a
Billy Li <billyli@me.com>
2016-11-17 14:06:55 +0800

    event update

--- src/app/vendor.js
+++ src/app/vendor.js
@@ -5,4 +5,5 @@ import '../../node_modules/ngprogress-lite/ngprogress-lite.css';
 import '../../node_modules/ui-select/dist/select.css';
 import '../../node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css';
 import '../../node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css';
+import '../../node_modules/ng-table/bundles/ng-table.css';
 import '../assets/stylesheets/main.scss';
