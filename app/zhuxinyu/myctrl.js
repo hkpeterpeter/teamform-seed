c9557e79d28368b9347e3c63932a756b383fd528
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-11-18 20:02:37 +0800

    Update from milestone 2

--- app/zhuxinyu/myctrl.js
+++ app/zhuxinyu/myctrl.js
@@ -39,11 +39,7 @@ teamapp.controller('search_controll', ['$scope',"$rootScope","allteams","alleven
 
     $scope.createflip = function() {
         if ($scope.event.name != "") {
-            try{
             document.getElementById('myflipper').classList.toggle('flipped');
-            }catch(err){
-                return "NONE";
-            }
             
         } else {
             Materialize.toast('Please Enter The Event Name!', 1000);
@@ -54,17 +50,14 @@ teamapp.controller('search_controll', ['$scope',"$rootScope","allteams","alleven
     $scope.cancelEvent = function() {
 
         $scope.event.name="";
-        try{
         document.getElementById('myflipper').classList.toggle('flipped');
-        }catch(err){
-            return "ERR";
-        }
     };
 
- 
+  
 
     $scope.searchEvent = function() {
-     
+      
+
         if($scope.event.name!=""){
             var resultList=[];
             for(var i=0;i<$rootScope.events.length;i++){
@@ -74,49 +67,40 @@ teamapp.controller('search_controll', ['$scope',"$rootScope","allteams","alleven
                     resultList.push($rootScope.events[i]);
                 }
             } 
-           
+         
             $scope.updateEventList(resultList);
-            return "SUCCESS";
            
         }else{
              Materialize.toast('Please Enter The Event Name!', 1000);
-             return "FAIL";
         }
     }
-    $scope.deleteChild=function(eventlist){
-        $("#eventCardList").children().remove();
 
-
-        for(var i=0;i<eventlist.length;i++){
-            try{
-            $rootScope.addEventCard(eventlist[i]);
-            }catch(err){};
-        }
-        $("#eventCardList").hide();
-
-        $("#searching").fadeOut(1000,function(){
-        $("#eventCardList").show(1000);
-
-        });       
-    }
     $scope.updateEventList=function(eventlist){
         if(eventlist.length>0){
-            try{
             $('html, body').animate({
             scrollTop: $("#event_list").offset().top
             }, 1000);
-            }catch(err){};
           $("#searching").show();
           
            $("#eventCardList").children().hide(1000,function(){
-                $scope.deleteChild(eventlist);
 
-               
+
+                 $("#eventCardList").children().remove();
+
+
+                for(var i=0;i<eventlist.length;i++){
+                  
+                    $rootScope.addEventCard(eventlist[i]);
+                }
+                 $("#eventCardList").hide();
+
+                  $("#searching").fadeOut(1000,function(){
+                     $("#eventCardList").show(1000);
+
+                  });       
            });
-           return "SUCCESS";
        }else{
             Materialize.toast("Sorry We didn't find your event! You may create this event.", 3000);
-             return "Fail";
        }
 
       
@@ -146,9 +130,8 @@ teamapp.directive("footerPanel",function(){
         transclude: true,
         scope:{
             ftitle:"@",
-            currenpic:"@"
+            
         }
-
     };
 
 });
@@ -198,33 +181,41 @@ teamapp.directive('eventCard', function($compile) {
 
             };
 
-            $rootScope.goEvent=function(){
-                $scope.goToEvent();
-            };
 
-            $rootScope.checkUser=function(data){
+            $scope.goToEvent =function(){
+
+          
+                 
+                $firebaseObject($rootScope.event_ref.child($scope.eid)).$bindTo($rootScope,"bindedclickedEvent");
+
+                $rootScope.clickedEvent=$firebaseObject($rootScope.event_ref.child($scope.eid));
+               
+                $rootScope.clickedEvent.$loaded().then(function(data){
+                    console.log(data);
+                    console.log($rootScope.currentUser.id);
+
                 if($rootScope.currentUser.id==data.adminID){
                 //is Admin
 
-                   
+                    console.log("An Admin");
                     window.location.href = '#/admin';
                 }else{
                     var isLeader=false;
                     var isMember=false;
 
-                 
+                    console.log("All teams are "+data.allTeams);
 
                     for(var i=0;i<data.allTeams.length;i++){
                         if(data.allTeams[i].leader==$rootScope.currentUser.id){
                             isLeader=true;
-                         
+                            console.log("a leader");
                              window.location.href = '#/teamleader';
                            return;
                         }
                        for(var j=0;j<data.allTeams[i].member.length;j++){
                             if(data.allTeams[i].member[j]==$rootScope.currentUser.id){
                                 isMember=true;
-                            
+                                console.log("a member");
                                 window.location.href = '#/team';
                               return;
                             }
@@ -232,35 +223,18 @@ teamapp.directive('eventCard', function($compile) {
 
                     }
                     if(!(isLeader||isMember)){
-                     
+                        console.log("a people");
                         window.location.href = '#/eventx';
                     }
 
                 
 
                 }
-            };
-            $scope.goToEvent=function(){
 
-          
-                 try{
-                $firebaseObject($rootScope.event_ref.child($scope.eid)).$bindTo($rootScope,"bindedclickedEvent");
-                }catch(err){
 
-                }
-                 try{
-                $rootScope.clickedEvent=$firebaseObject($rootScope.event_ref.child($scope.eid));
-               }catch(err){
-                    $rootScope.clickedEvent={};
-               }
+                },function(err){
 
-               try{
-                $rootScope.clickedEvent.$loaded().then(function(data){
-                    $rootScope.checkUser(data);
                 });
-            }catch(err){
-                   
-            }
               
                
 
@@ -282,27 +256,7 @@ teamapp.directive("subcan", function() {
         restrict: "E",
         templateUrl: "zhuxinyu/js/components/submitCancelPanel/subcan.html",
         controller: function ($rootScope,$scope, $element,$firebaseObject,$firebaseArray,allteams,allevents,allusers) {
-            $rootScope.processRef=function(ref){
-                 var eventID=ref.key;
-         
-                 try{
-                $firebaseArray($rootScope.user_ref.child($rootScope.currentUser.id).child("eventsManaging")).$add(eventID);
-                }catch(err){}
 
-                try{
-                $firebaseArray($rootScope.user_ref.child($rootScope.currentUser.id).child("notifs")).$add({
-                    content: "An new event "+ $scope.event.name +" has been created",
-                    read: false,
-                    type:"System"
-                });
-                }catch(err){}
-
-                 Materialize.toast("Your new event "+$scope.event.name+" has been created", 3000);
-
-               
-                $scope.cancelEvent();
-                return eventID;
-            }
             $scope.createEvent=function(){
                 
                 var eventCreating={};
@@ -310,32 +264,29 @@ teamapp.directive("subcan", function() {
                 eventCreating.description=$scope.eventDescription;
                 eventCreating.min_num=$scope.eventMin;
                 eventCreating.max_num=$scope.eventMax;
-                eventCreating.evnetName=$scope.event.name;
-              
-                 try{
-                 eventCreating.imageUrl=$rootScope.currentUser.profilePic;
-               
-                }catch(err){
-                   eventCreating.imageUrl="abc.jpg";
-                }
-
-            
-
-                try{
                 eventCreating.adminID=$rootScope.currentUser.id;
-               
-                }catch(err){
-                    eventCreating.adminID="0";
-                }
-                
-                try{
+                eventCreating.evnetName=$scope.event.name;
+                eventCreating.imageUrl=$rootScope.currentUser.profilePic;
+                console.log(eventCreating);
 
                 $rootScope.events.$add(eventCreating).then(function(ref){
+                    var eventID=ref.key;
+                    console.log(eventID);
 
-                    $rootScope.processRef(ref);
-               
+                    $firebaseArray($rootScope.user_ref.child($rootScope.currentUser.id).child("eventsManaging")).$add(eventID);
+
+
+                    $firebaseArray($rootScope.user_ref.child($rootScope.currentUser.id).child("notifs")).$add({
+                        content: "An new event "+ $scope.event.name +" has been created",
+                        read: false,
+                        type:"System"
+                    });
+
+                     Materialize.toast("Your new event "+$scope.event.name+" has been created", 3000);
+
+                   
+                    $scope.cancelEvent();
                 });
-            }catch(err){}
             }
 
         }
@@ -367,11 +318,8 @@ teamapp.directive("zhuNavi", function() {
            
              try{
 
-           $firebaseObject($rootScope.user_ref.child($rootScope.currentUser.id).child("notifs")).$bindTo($scope,"allNotif");
-       }catch(err){
-            $scope.allNotif={1:{type:"invitation",content:"You are invited",read:false},2:{type:"System",content:"You are invited",read:false},3:{type:"System",content:"You are invited",read:true}};
-       }
-        try{
+            $firebaseObject($rootScope.user_ref.child($rootScope.currentUser.id).child("notifs")).$bindTo($scope,"allNotif");
+
             $scope.shownotify=function(){
                 $scope.ntList=[];
 
@@ -437,7 +385,9 @@ teamapp.directive("invitationBar",function(){
         },
         transclude:true,
         controller: function ($rootScope,$scope,$firebaseObject,$firebaseArray) {
-           
+            $scope.accept=function(){
+                console.log("User:"+$rootScope.currentUser.id+" should be added to team "+$scope.team);
+            }
         }
     }
 });
