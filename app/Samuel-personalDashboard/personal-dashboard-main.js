15d091076bf73adb8b457f90fcc056607fbd30c0
He Zhou <hezhou556@gmail.com>
2016-11-18 22:48:05 +0800

    Un-comment those difficult functions

--- app/Samuel-personalDashboard/personal-dashboard-main.js
+++ app/Samuel-personalDashboard/personal-dashboard-main.js
@@ -70,44 +70,44 @@ teamapp.controller("dashboardController", function ($rootScope, $scope, $firebas
     var notifsRef = userRef.child('/notifs');
     $scope.notifs = $firebaseArray(notifsRef);
 
-    // $scope.acceptInvitation = function(index){  //TODO: check whether this team is full
-    //     //To add him to the member list
-    //     var thisteamMemberList = $firebaseArray( firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/membersID') );
-    //     thisteamMemberList.$loaded().then(function(){
-    //         console.log("this team before adding him, has " + thisteamMemberList);
-    //         thisteamMemberList.$add($rootScope.currentUser.id);
-    //     });
-    //
-    //     //To remove him from the "invited people" list
-    //     var thisteamInvitedPeople = $firebaseArray(firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/invitedPeople'));
-    //     thisteamInvitedPeople.$loaded().then(function(){
-    //         for (var i = 0; i<thisteamInvitedPeople.length; i++){
-    //             if (thisteamInvitedPeople[i].$value === $rootScope.currentUser.id){
-    //                 thisteamInvitedPeople.$remove(i);
-    //             }
-    //         }
-    //     });
-    //
-    //     //To update his own copy of "teams as member"
-    //     $scope.memberList.$add($scope.invitedList[index].$value);
-    //
-    //     //To remove this team from his "being invited list"
-    //     $scope.invitedList.$remove(index);
-    // };
-    //
-    // $scope.turndownInvitation = function(index){
-    //
-    //     //To remove him from the "invited people" list
-    //     var thisteamInvitedPeople = $firebaseArray(firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/invitedPeople'));
-    //     thisteamInvitedPeople.$loaded().then(function(){
-    //         for (var i = 0; i<thisteamInvitedPeople.length; i++){
-    //             if (thisteamInvitedPeople[i].$value === $rootScope.currentUser.id){
-    //                 thisteamInvitedPeople.$remove(i);
-    //             }
-    //         }
-    //     });
-    //     $scope.invitedList.$remove(index);
-    // }
+    $scope.acceptInvitation = function(index){  //TODO: check whether this team is full
+        //To add him to the member list
+        var thisteamMemberList = $firebaseArray( firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/membersID') );
+        thisteamMemberList.$loaded().then(function(){
+            console.log("this team before adding him, has " + thisteamMemberList);
+            thisteamMemberList.$add($rootScope.currentUser.id);
+        });
+
+        //To remove him from the "invited people" list
+        var thisteamInvitedPeople = $firebaseArray(firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/invitedPeople'));
+        thisteamInvitedPeople.$loaded().then(function(){
+            for (var i = 0; i<thisteamInvitedPeople.length; i++){
+                if (thisteamInvitedPeople[i].$value === $rootScope.currentUser.id){
+                    thisteamInvitedPeople.$remove(i);
+                }
+            }
+        });
+
+        //To update his own copy of "teams as member"
+        $scope.memberList.$add($scope.invitedList[index].$value);
+
+        //To remove this team from his "being invited list"
+        $scope.invitedList.$remove(index);
+    };
+
+    $scope.turndownInvitation = function(index){
+
+        //To remove him from the "invited people" list
+        var thisteamInvitedPeople = $firebaseArray(firebase.database().ref('/teams/'+$scope.invitedList[index].$value + '/invitedPeople'));
+        thisteamInvitedPeople.$loaded().then(function(){
+            for (var i = 0; i<thisteamInvitedPeople.length; i++){
+                if (thisteamInvitedPeople[i].$value === $rootScope.currentUser.id){
+                    thisteamInvitedPeople.$remove(i);
+                }
+            }
+        });
+        $scope.invitedList.$remove(index);
+    }
 
 
 });
