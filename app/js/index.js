93d6cc38d98404a225c31e2c53cf82af9c285088
Raymond Lui <lui2345@yahoo.com.hk>
2016-11-23 18:40:01 +0800

    Added all events, teams and members navigation in index.html Users can directly send request to teams directly from index page, jump to admin, team and member page from the navigation pane without typing event name in the top input

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,7 @@ function adminBtnClick() {
     var val = $('#input_text').val();
     if(val !== '') {
         var url = "admin.html?q=" + val;
-        window.location.href= url ;
+        window.location.href = url;
         return false;
     }
 }
@@ -11,7 +11,7 @@ function leaderBtnClick() {
     var val = $('#input_text').val();
     if(val !== '') {
         var url = "team.html?q=" + val;
-        window.location.href= url ;
+        window.location.href = url;
         return false;
     }
 }
@@ -20,7 +20,7 @@ function memberBtnClick() {
     var val = $('#input_text').val();
     if(val !== '') {
         var url = "member.html?q=" + val;
-        window.location.href= url ;
+        window.location.href = url;
         return false;
     }
 }
@@ -43,14 +43,6 @@ $(document).ready(function() {
         }
     });
 
-    var eventDetail = document.getElementById('eventDetail');
-
-    window.onclick = function(event) {
-        if(event.target == eventDetail) {
-            eventDetail.style.display = "none";
-        }
-    };
-
 });
 
 angular.module('teamform-app', ['firebase'])
@@ -60,6 +52,8 @@ angular.module('teamform-app', ['firebase'])
         templateUrl: 'login.html'
     };
 }).controller('indexCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+    var uid = "";
+
     $scope.dteam = [
         {name: "Jacky CHEUNG", email: "cycheungar@connect.ust.hk"},
         {name: "Carson CHOW", email: "ccchowae@connect.ust.hk"},
@@ -72,7 +66,6 @@ angular.module('teamform-app', ['firebase'])
     $scope.pageNum = 1;
 
     $scope.eventList = $firebaseArray(firebase.database().ref().child("eventList"));
-    console.log($scope.eventList);
 
     $scope.activeEventList = [];
     $scope.eventList.$loaded().then(function(data) {
@@ -98,14 +91,28 @@ angular.module('teamform-app', ['firebase'])
                 });
             }
         });
-        console.log($scope.activeEventList);
     });
-    $scope.joinEvent = function(eventName) {
-        window.location.href = "admin.html?q=" + eventName;
+    $scope.joinEvent = function(eventName, mode) {
+        uid = firebase.auth().currentUser.uid;
+        //check the user has already joined the team (the child member should have his/her record)
+        var eventRef = firebase.database().ref().child(eventName).child("member").child(uid);
+        var eventRefObj = $firebaseObject(eventRef);
+        eventRefObj.$loaded().then(function() {
+            if(typeof eventRefObj.weight === "undefined") {    // new joiner
+                eventRef.set({weight: 0});
+            }
+        });        
+        if(mode === 'admin') {
+            window.location.href = "admin.html?q=" + eventName;
+        }
+        else if(mode === 'member') {
+            window.location.href = "member.html?q=" + eventName;
+        }
     };
 
     $scope.teamList = [];
     $scope.showEventDetail = function(eventName) {
+        $scope.eventName = eventName;
         $scope.teamList = [];
         document.getElementById('eventDetail').style.display = 'block';
         var eventRef = firebase.database().ref().child(eventName);
@@ -120,6 +127,95 @@ angular.module('teamform-app', ['firebase'])
                 };
                 $scope.teamList.push(teamInfo);
             });
+        });
+    };
+
+    $scope.memberList = [];
+    $scope.displayMembersInTeam = function(teamName, eventName) {
+        $scope.teamName = teamName;
+        $scope.memberList = [];
+        document.getElementById('memberlist').style.display='block';
+        var memberArray = $firebaseArray(firebase.database().ref().child(eventName).child("member"));
+        var teamRef = firebase.database().ref().child(eventName).child("team").child(teamName);
+        var teamMembersObj = $firebaseObject(teamRef.child("teamMembers"));
+        teamMembersObj.$loaded().then(function(data) {
+            angular.forEach(data, function(teamMember) {
+                var memberInfo = {};
+                var memRecord = memberArray.$getRecord(teamMember);
+                var memberName = $firebaseObject(firebase.database().ref().child("user").child(teamMember));
+                memberName.$loaded().then(function() {
+                    if(memRecord != null) {
+                        memberInfo.name = memberName.name;
+                        memberInfo.tags = memRecord.tags;
+                    }
+                });
+                $scope.memberList.push(memberInfo);
+            });
+        });
+        uid = firebase.auth().currentUser.uid;
+        var memberInfo = $firebaseObject(firebase.database().ref().child($scope.eventName).child("member").child(uid));
+        memberInfo.$loaded().then(function() {
+            if(memberInfo.inTeam === $scope.teamName) {
+                $("#manageTeam").html("<button type=\"button\" class=\"btn btn-primary\" id=\"manageTeamBtn\">Manage</button>");
+                $("#manageTeamBtn").click(manageTeam);
+            }
+            else {
+                $("#manageTeam").html("");
+            }
+        });
+    };
+
+    var manageTeam = function() {
+        window.location.href = "team.html?q=" + $scope.eventName;
+    };
+
+    $scope.createNewEvent = function() {
+        uid = firebase.auth().currentUser.uid;
+        if($scope.newEvent !== "") {
+            //create a new event with name, event admin, max/min teamsize initialized
+            //1. add the event to eventlist
+            firebase.database().ref("eventList/" + $scope.newEvent).set({status: "active"});
+            //2. add the event info to firebase
+            var eventParam = $firebaseObject(firebase.database().ref().child($scope.newEvent).child("admin").child("param"));
+            eventParam.maxTeamSize = 10;
+            eventParam.minTeamSize = 1;
+            eventParam.eventAdmin = uid;
+            eventParam.$save();
+            //3. add the event admin to one of the member
+            firebase.database().ref().child($scope.newEvent).child("member").child(uid).set({weight: 0}).then(function() {
+                window.location.href = "admin.html?q=" + $scope.newEvent;
+            });
+        }
+    };
+
+    $scope.joinTeam = function(teamName, eventName) {
+        uid = firebase.auth().currentUser.uid;
+        var memberRef = firebase.database().ref().child(eventName).child("member").child(uid);
+        var memberRefObj = $firebaseObject(memberRef);
+        memberRefObj.$loaded().then(function() {
+            if(typeof memberRefObj.inTeam !== "undefined") {
+                alert("You are already in team " + memberRefObj.inTeam + "!");
+            }
+            else if(confirm("Are you sure to request to join the team: " + teamName + " in event " + eventName + "?")) {            
+                if(typeof memberRefObj.weight === "undefined") {
+                    memberRef.set({weight: 0});
+                }
+                else if(typeof memberRefObj.selection !== "undefined") {
+                    angular.forEach(memberRefObj.selection, function(team) {
+                        if(team === teamName) {
+                            alert("You have sent the request to this team already!");
+                            return;
+                        }
+                    });
+                    memberRefObj.selection.push(teamName);
+                    memberRefObj.$save();
+                }
+                else {
+                    var teamSelection = [teamName];
+                    memberRef.update({selection: teamSelection});
+                }
+                alert("Request sent to team " + teamName);
+            }
         });
     };
 
