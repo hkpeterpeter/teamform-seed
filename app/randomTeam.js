a0aa3d8f73e552926d189bed0858a07aa86993e4
KUEN\cckhhu6495 <cckhhu6495@gmail.com>
2016-11-27 00:13:02 +0800

    delete other unuse random method

--- app/randomTeam.js
+++ app/randomTeam.js
@@ -16,20 +16,7 @@ app.controller("random_Team",["$scope","$firebaseArray", "Auth","$rootScope",
 		};
 		
 		
-		/*var shuffleArray = function(array) {
-			var m = array.length, t, i;
-			// While there remain elements to shuffle
-			while (m) {
-				// Pick a remaining element…
-				i = Math.floor(Math.random() * m--);
-					// And swap it with the current element.
-				t = array[m];
-				array[m] = array[i];
-				array[i] = t;
-			}
-			return array;
-		}*/
-		//shuffleArray($scope.teamOfChosenEvent);
+
 		
 	}	
 ]);
