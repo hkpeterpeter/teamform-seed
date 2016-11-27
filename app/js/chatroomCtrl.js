390821d37fc79a7fcf10201c86d09dc3439be78f
Tim <ctlamaf@connect.ust.hk>
2016-11-28 06:17:24 +0800

    complete chatroom

--- app/js/chatroomCtrl.js
+++ app/js/chatroomCtrl.js
@@ -1,66 +1,152 @@
 var app = angular.module("chatroomApp", ["firebase"]);
 
 app.controller("chatroomCtrl",
-
-    function ($scope, $firebaseArray) {
+    function ($scope, $firebaseArray, $timeout) {
         var ref = firebase.database().ref("chats");
         $scope.chatrooms = $firebaseArray(ref);
+        $scope.members = $firebaseArray(firebase.database().ref("members"));
+        $scope.user = null;
+        $scope.userID = "";
+        $scope.msg = "";
+        $scope.currentConversation = null;
+        $scope.readableTime = Utils.getDateAndTimeString;
+        var lastPos = 0;
 
-        $scope.tempcontent = "";
+        function record(_sender, _message) {
+            this.sender = _sender;
+            this.message = _message;
+            this.time = new Date().toUTCString();
+        }
 
-        $scope.hasChatRecord = function (currentMemberList) {
-            
-        var checking = true;
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                $scope.user = user;
+                $scope.members.$loaded().then(function () {
+                    angular.forEach($scope.members, function (e) {
+                        if (e.uid == $scope.user.uid)
+                            $scope.userID = e.id;
+                    });
+                });
+            } else {
+                // No user is signed in.
+                showLoginDialog();
+            }
+        });
 
-        for(var t=0;t<$scope.chatrooms.length;t++){
-             for(var i=0;i<$scope.chatrooms[t].members.length && checking ==true;i++){
-                 if($scope.chatrooms[t].members.length==1 && $scope.chatrooms[t].members[i]!=currentMemberList[i]){
-                     break;
-                    }
+        function scrolling() {
+            scrollToBottom();
+            if ($("#left-top-panel").scrollTop() != lastPos) {
+                lastPos = $("#left-top-panel").scrollTop();
+                if (lastPos > 0)
+                    return;
+            }
+            $timeout(scrolling, 200);
+        }
+        scrolling();
 
-                 for(var j=0;j<currentMemberList.length;j++){
-                    if($scope.chatrooms[t].members[i]==currentMemberList[j]){
+        $scope.getNameByID = function (id) {
+            for (var i = 0; i < $scope.members.length; i++)
+                if ($scope.members[i].id == id)
+                    return $scope.members[i].first_name;
+        }
+
+        $scope.send = function () {
+            $scope.user = firebase.auth().currentUser;
+            if (!$scope.user || !$scope.currentConversation || $scope.msg == "")
+                return;
+
+            $scope.chatrooms.$loaded().then(function () {
+                var m = new record($scope.userID, $scope.msg);
+
+                for (var i = 0; i < $scope.chatrooms.length; i++) {
+                    if ($scope.chatrooms[i].list.length != $scope.currentConversation.list.length)
+                        continue;
+                    var exist = true;
+                    for (var j = 0; j < $scope.chatrooms[i].list.length; j++)
+                        exist &= Utils.arrayContains($scope.chatrooms[i].list[j], $scope.currentConversation.list);
+                    if (exist) {
+                        $scope.chatrooms.$ref().child(i).child("messages").child($scope.chatrooms[i].messages.length).set(m);
                         break;
                     }
+                }
+                $scope.msg = "";
+                $("#left-bottom-panel input[type='text']").focus();
+                scrolling();
+            });
+        };
 
-                    if($scope.chatrooms[t].members[i]!=currentMemberList[j] && (j == (currentMemberList.length-1))){
-                        checking =false;
-                        break;
+        $scope.createConversation = function () {
+            $scope.user = firebase.auth().currentUser;
+            if (!$scope.user)
+                return;
+
+            $scope.chatrooms.$loaded().then(function () {
+                $scope.members.$loaded().then(function () {
+                    var list1 = prompt("Please input the IDs you want to add into the group.\nSplit the IDs with comma (e.g. ID_1, ID_2)");
+                    var name1 = prompt("Please input a name for the chat group.");
+                    list1 = list1.split(" ").join("");
+                    list1 = list1.split(",");
+                    list1.push($scope.userID);
+
+                    var alreadyExist = false;
+                    for (var i = 0; i < $scope.chatrooms.length && !alreadyExist; i++) {
+                        if ($scope.chatrooms[i].list.length != list1.length)
+                            continue;
+                        var exist = true;
+                        for (var j = 0; j < $scope.chatrooms[i].list.length; j++)
+                            exist &= Utils.arrayContains($scope.chatrooms[i].list[j], list1);
+                        if (exist)
+                            alreadyExist = true;
                     }
-            }
-            if(i==($scope.chatrooms[t].members.length-1)){
-                    return t;}
-            }
-        }
-            return -1;
-        }
 
-        $scope.addChatRecord = function () {
-            //$scope.input.members = $scope.input.members.split(" ").join("").split(",");
-            var currentMemberList = ["0001","0005"];
-            var index = $scope.hasChatRecord(currentMemberList);
+                    var tempCount = 0;
+                    for (var i = 0; i < $scope.members.length; i++) {
+                        if (Utils.arrayContains($scope.members[i].id, list1))
+                            tempCount++;
+                    }
 
-            if (index >= 0) {
-                $scope.chatrooms.$ref().child(index).child("msgs").child($scope.chatrooms[index].msgs.length).set({
-                    "content": $scope.tempcontent,
-                    "date": new Date().toString()
+                    if (tempCount < list1.length)
+                        alert("At least one of the IDs is not valid!");
+                    else if (alreadyExist)
+                        alert("This conversation has already exist!");
+                    else {
+                        var obj = {
+                            name: name1,
+                            list: list1,
+                            messages: [{
+                                sender: "System",
+                                message: "Welcome to this new chat group!",
+                                time: new Date().toUTCString()
+                            }]
+                        };
+                        $scope.chatrooms.$ref().child($scope.chatrooms.length).set(obj);
+                    }
                 });
-            }
-            else {
-                $scope.chatrooms.$ref().child($scope.chatrooms.length).set({
-                    "members": currentMemberList,
-                    "msgs": [{ "content": $scope.tempcontent, "date": new Date().toString() }]
-                });
-            }
-
+            });
         }
 
-
-        $scope.addNewMessage = function () {
-            if ($scope.tempcontent != "") {
-                $scope.addChatRecord();
-            }
-        }
-
+        $scope.showConversation = function (conversation) {
+            $scope.currentConversation = conversation;
+        };
     }
 );
+
+function showLoginDialog() {
+    $("#loginDialog").removeClass("hide");
+}
+
+function hideLoginDialog() {
+    $("#loginDialog").addClass("hide");
+}
+
+function scrollToBottom() {
+    $("#left-top-panel").scrollTop($("#left-top-panel")[0].scrollHeight);
+}
+
+$(document).ready(function () {
+    $("#left-bottom-panel input[type='text']").keypress(function (event) {
+        if (event.which == 13)
+            $("#left-bottom-panel input[type='button']").click();
+    });
+});
