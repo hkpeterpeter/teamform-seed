8c0f0d892834349086cbc66295b1ae110d9f75f0
mnlauaa <mnlauaa@connect.ust.hk>
2016-11-27 21:11:27 +0800

    no message

--- app/login/admin/save.js
+++ app/login/admin/save.js
@@ -1,11 +1,11 @@
-app.controller("MyCtrl", ["$scope", "$firebaseObject",
-  function($scope, $firebaseObject) {
+app.controller("MyCtrl", ["$scope", "$firebaseObject", "$firebaseArray"
+  function($scope, $firebaseObject, $firebaseArray) {
      var ref = firebase.database().ref();
 
      var obj = $firebaseObject(ref);
 
      // to take an action after the data loads, use the $loaded() promise
-     obj.$loaded().then(function() {
+     obj.$loaded().then(function() { });
         console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
 
        // To iterate the key/value pairs of the object, use angular.forEach()
