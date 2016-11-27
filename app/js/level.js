6860200b495034b610ef2f57b439e53fd0109a73
Howe <khm1229@gmail.com>
2016-11-27 19:52:13 +0800

    #(!_$&#(%&#(!_%&_!

--- app/js/level.js
+++ app/js/level.js
@@ -1,26 +1,40 @@
 // inject firebase service
 var app = angular.module("level", ["firebase"]); 
 
-app.controller("levelCtrl", 
+app.controller("levelCtrl",  
 
 	// Implementation the todoCtrl 
-	function($rootScope, $scope, $firebaseArray) {
-
-        $scope.percent = "66.6%";
-        var el = document.getElementById("circle");
-        el.setAttribute("value", $scope.percent);
-        compile(el);    
-
+	function($scope, $firebaseArray,$window,$timeout) {
+        $scope.current = 0;
+        $scope.total = 100;
+        value = '0%';
 		var ref=firebase.database().ref("pi");
     	ref.orderByChild("Name").equalTo("Kit").once("child_added", function(dataRef) {
             $scope.$apply(function() {
             $scope.level = dataRef.child("level").val();
             $scope.current = dataRef.child("current").val();
             $scope.total = dataRef.child("total").val();
-        	});
+            $scope.value = $scope.current/$scope.total*100 + "%";
+            knobfunction($scope.value);
+           });
         });
         
-	}
+        $scope.add = function (){
+			$scope.current+=10;
+            $scope.value = ($scope.current)/$scope.total*100 +"%";
+            knobfunction($scope.value);
+
+        };
+        
+        //
+        //$scope.$watch('total', function() {
+        ////    $scope.percent = $scope.current/$scope.total*100 + "%";
+        //    var el = document.getElementById("circle");
+        //  //  el.setAttribute("value", $scope.percent);
+        //    compile(el);
+        //});
+
+    }
 );
 
 function compile(element){
