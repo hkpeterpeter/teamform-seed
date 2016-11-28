b58050a957de81310e8e58d9b8877891077b8d02
a4n0nym0u5 <hades94720@gmail.com>
2016-11-28 11:30:11 +0800

    temp_integraate

---
+++ app/temp_integrate/app.js
@@ -0,0 +1,198 @@
+var app = angular.module("teamApp", ["ui.router","firebase"]);
+
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+          var ref = firebase.database().ref();
+    return $firebaseAuth();
+  }
+]);
+
+app.directive('quiz', function(quizFactory,$state) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'template.html',
+		link: function(scope, elem, attrs) {
+			scope.start = function() {
+				scope.id = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.typeid=1;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+					scope.typeid=1;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+
+			scope.checkAnswer = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+					scope.typeid=1;
+				} else {
+					scope.correctAns = false;
+				}
+
+				scope.answerMode = false;
+			};
+
+scope.check = function() {
+				console.log("haha");
+			};
+
+scope.exit = function() {
+				$state.go('teamsearch');
+			};
+
+			scope.nextQuestion = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+				} else {
+					scope.correctAns = false;
+				}
+				scope.id++;
+				scope.typeid=1;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question: "I love to take care of people and I'm good at it?",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{
+			question: "I tend to trust most people.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{
+			question: "I am too strict with myself and others.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{
+			question: "I am more sensitive than most people; sometimes the world just seems too harsh.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "Success, prestige and recognition really matter to me.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{	
+			question: "I am always aware of what needs to be corrected.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "I am uncomfortable when people want an emotional response from me.",
+			options: ["No", "Partial", "Yes"],
+			answer: 0
+		},
+		{	
+			question: "I am more organized than most.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{	
+			question: "Personal relationships are by far the most important thing in my life.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{	
+			question: "I get bored more easily than most people; I am always looking for new experiences.",
+			options: ["No", "Partial", "Yes"],
+			answer: 0
+		},
+		{	
+			question: "I am more formal than most people.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{	
+			question: "I am highly individualistic.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "Sometimes I have overextended myself in trying to help people.",
+			options: ["No", "Partial", "Yes"],
+			answer: 1
+		},
+		{	
+			question: "I tend to avoid conflict.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "I almost never lose control of myself.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "I am good at getting things done.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		},
+		{	
+			question: "It's hard to stay passionate and focused.",
+			options: ["No", "Partial", "Yes"],
+			answer: 0
+		},
+		{	
+			question: "I'm good at motivating people.",
+			options: ["No", "Partial", "Yes"],
+			answer: 2
+		}
+
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
+
+// Controller
+$scope.isLast = function(check) {
+    var cssClass = check ? 'last' : null;
+    return cssClass;
+};
---
+++ app/temp_integrate/app.route.js
@@ -0,0 +1,159 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+ 
+    $urlRouterProvider.otherwise('/main');
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+
+        .state('login', {
+            url: '/login',
+            templateUrl: 'login.html',
+            controller: 'AuthCtrl',
+            resolve: {
+      // controller will not be loaded until $waitForSignIn resolves
+      // Auth refers to our $firebaseAuth wrapper in the factory below
+      "currentAuth": ["Auth", function(Auth) {
+        // $waitForSignIn returns a promise so the resolve waits for it to complete
+        return Auth.$waitForSignIn();
+      }]
+    }
+        })
+        
+         .state('register', {
+            url: '/register',
+            templateUrl: 'register.html',
+            controller: 'AuthCtrl'
+        })
+
+.state('main', {
+            url: '/main',
+            templateUrl: 'main2.html'
+        })
+
+ .state('personality', {
+            url: '/personality',
+            templateUrl: 'typequiz.html'
+            })
+
+            .state('level', {
+            url: '/level',
+            templateUrl: 'level.html'
+            })
+
+.state('nearestpeople', {
+            url: '/nearestpeople',
+            templateUrl: 'nearestpeople.html'
+            })
+.state('iqtest', {
+            url: '/iqtest',
+            templateUrl: 'IQtest.html'
+            })
+
+.state('eventjoin', {
+            url: '/eventjoin',
+            templateUrl: 'joinevent.html',
+            controller: 'eventjoin'
+            })
+
+            .state('subteam', {
+            url: '/subteam',
+            templateUrl: 'SubTeam.html'
+            })
+
+        .state('subteaminfo', {
+            url: '/subteaminfo',
+            templateUrl: 'SubTeaminformation.html'
+            })
+
+         .state('personal', {
+            url: '/personal',
+            templateUrl: 'personal_page.html',
+            controller: 'Personal',
+            authRequired: true,
+             })
+
+      .state('event', {
+            url: '/event',
+            templateUrl: 'event.html',
+            authRequired: true,
+            controller: 'eventSubmit'
+            })
+
+           
+            
+             .state('search', {
+            url: '/search',
+            templateUrl: 'search.html',
+            controller: 'eventSearch'
+            })
+
+            .state('teamsearch', {
+            url: '/teamsearch',
+            templateUrl: 'newsearch.html',
+            controller: 'eventSearch'
+            })
+        
+       .state('Teaminformation', {
+            url: '/Teaminformation',
+            templateUrl: 'Teaminformation.html',
+            controller: 'teaminfo'
+            })
+			
+	   .state('randomTeam', {
+            url: '/randomTeam',
+            templateUrl: 'randomTeam.html',
+            controller: 'random_Team'
+            })
+
+        .state('TerminateEvent', {
+            url: '/TerminateEvent',
+            templateUrl: 'terminate_event.html',
+            controller: 'eventterm'
+            })
+
+
+		.state('createTeam', {
+            url: '/createTeam',
+            templateUrl: 'createTeam.html',
+            authRequired: true,
+            controller: 'teamSubmit'
+            })
+
+    .state('editteaminfo', {
+            url: '/editteaminfo',
+            templateUrl: 'editteaminfo.html',
+            controller: 'teamedit'
+            })
+
+             .state('kickmember', {
+            url: '/kickmember',
+            templateUrl: 'kick.html',
+            controller: 'kick'
+            })
+            
+             .state('privatepublic', {
+            url: '/privatepublic',
+            templateUrl: 'publicprivate.html',
+            controller: 'openness'
+            })
+
+             .state('personal_information', {
+            url: '/personal_information',
+            templateUrl: 'personal_information.html',
+            authRequired: true,
+            controller: 'piCtrl'
+            })
+             
+             .state('requests', {
+            url: '/handleRequests',
+            templateUrl: 'handleRequests.html',
+            controller: 'handleRequests',
+            })
+			
+			.state('randomEvent', {
+            url: '/randomEvent',
+            templateUrl: 'randomEvent.html',
+            controller: 'random_Event',
+            });
+});
+
