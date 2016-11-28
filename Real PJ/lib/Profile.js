41d69f0ef4043c34102a21f7f5eaa4c53919fe5e
mnlauaa <mnlauaa@connect.ust.hk>
2016-11-28 17:42:57 +0800

    no message

--- Real PJ/lib/Profile.js
+++ Real PJ/lib/Profile.js
@@ -1,7 +1,9 @@
 app.controller("profileCtrl", ["$scope", "$firebaseObject", "$firebaseArray",
   function($scope, $firebaseObject, $firebaseArray) {
-  		$scope.changeModel = function() {
-  			console.log("Signed in as:");
+  		$scope.EditProfile = function() {
+  			ref = firebase.database().ref('User/' + $scope.id);
+  			ref.update($scope.editProfile);
+  			
 			$("#editProfileModal").modal("hide")
 		};
   }
