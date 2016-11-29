568ba9a3ba87fed1f879596c20d5ee496567df0c
Howe <khm1229@gmail.com>
2016-11-29 17:34:36 +0800

    //

--- app/ui/join.js
+++ app/ui/join.js
@@ -1,15 +1,146 @@
+//app.controller("joinTeam",
+//               
+//    function($scope,$state){
+//                
+//            var eventRef=firebase.database().ref("events");
+//            var memberNoTeamRef=firebase.database().ref("memberWithNoTeam");
+//            
+//            var movingMember = {
+//                name:"",
+//                intro:"",
+//                team:"",
+//                uid:""
+//                };
+//        
+//            var newRequest = {
+//                eventName : "",
+//                targetTeam : "",
+//                memberName : "",
+//                message : "",
+//                receiver : ""
+//
+//            };
+//            
+//            
+//            
+//            $scope.joinTeamInPerson = function (event,teamName){
+//                movingMember="";
+//                
+//                    memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
+//                        function(oldLocation){
+//                            movingMember=oldLocation.val();
+//                            eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
+//                                targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(newLocation){
+//                                    newLocation.child("member").ref.push().set(movingMember);
+//                                    newLocation.ref.update({numberOfmember: newLocation.val().numberOfmember+1});
+//                                    oldLocation.ref.remove();
+//                                    window.alert("Join team success!");
+//
+//                                });
+//                
+//                            });
+//                
+//                        });
+//                
+//                
+//                
+//            
+//            };
+//            
+// $scope.joinTeamInPerson = function (event,teamName){
+//                movingMember="";
+//                
+//                    memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
+//                        function(oldLocation){
+//                            movingMember=oldLocation.val();
+//                            eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
+//                                targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(newLocation){
+//                                    newLocation.child("member").ref.push().set(movingMember);
+//                                    newLocation.ref.update({numberOfmember: newLocation.val().numberOfmember+1});
+//                                    oldLocation.ref.remove();
+//                                    window.alert("Join team success!");
+//
+//                                });
+//                
+//                            });
+//                
+//                        });
+//                
+//                
+//                
+//            
+//            };
+//
+//             $scope.test = function (){
+//                $state.go('personality');
+//                
+//
+//            };
+//            
+//
+//            $scope.reqJoinTeamInPerson = function (event,teamName){
+//                
+//              
+//                        
+//                        memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
+//                                function(exist)
+//                                {
+//                                        //if(exist.val()){
+//                                                //window.alert("found!");
+//                                                newRequest.eventName = event;
+//                                                newRequest.targetTeam = teamName;
+//                                                newRequest.memberName = exist.child("username").val();
+//                                                newRequest.message = $scope.reqMessage;
+//                                                
+//                                                eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
+//                                                        targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(targetTeam){
+//                                                             newRequest.receiver=targetTeam.child("teamleader").val();   
+//                                                        });
+//                                                });
+//                                                                
+//                                                
+//                                                
+//                                                
+//                                                
+//                                                firebase.database().ref("requests").push().set(newRequest);
+//        
+//                                                window.alert("Request sent!");
+//                                                newRequest.message="";
+//                                //        }
+//                                        //else {
+//                                        //        window.alert("You have a team!");
+//                                        //}
+//                                        
+//                                }                       
+//                        );
+//                   
+//            };  
+//    }                      
+//);
+//
+
+
 app.controller("joinTeam",
                
-    function($scope,$state){
+    function($scope){
                 
             var eventRef=firebase.database().ref("events");
             var memberNoTeamRef=firebase.database().ref("memberWithNoTeam");
+            var teamNoEvent=firebase.database().ref("TeamWithNoEvent");
             
             var movingMember = {
-                name:"",
+                username:"",
+                email:"",
                 intro:"",
                 team:"",
-                uid:""
+                uid:"",
+                gender: "",
+                phone:"",
+                birth: "",
+                position: "",
+                skill: "",
+                remark: "",
+                event: ""
                 };
         
             var newRequest = {
@@ -25,10 +156,12 @@ app.controller("joinTeam",
             
             $scope.joinTeamInPerson = function (event,teamName){
                 movingMember="";
-                
+                if(firebase.auth().currentUser){
                     memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
                         function(oldLocation){
                             movingMember=oldLocation.val();
+                            movingMember.team = teamName;
+                            movingMember.event = event;
                             eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
                                 targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(newLocation){
                                     newLocation.child("member").ref.push().set(movingMember);
@@ -42,45 +175,49 @@ app.controller("joinTeam",
                 
                         });
                 
-                
-                
+                }
+                else{
+                    window.alert("Please sign in first!");
+                }    
             
             };
             
- $scope.joinTeamInPerson = function (event,teamName){
+            $scope.joinTeamInTeam = function (event,teamName){
                 movingMember="";
+                if(firebase.auth().currentUser){
+                    //teamNoEvent.orderByChild("uid").on("child_added",
+                      //  function(t_team){
+                                
+                          teamNoEvent.ref.orderByChild("teamleader").equalTo(firebase.auth().currentUser.uid).once("child_added",
+                                function(t_team){
+                                        
+                                        eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
+                                                targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(newLocation){
+                                                        newLocation.child("member").ref.push().set(t_team.val());
+                                                        t_team.ref.remove();
+                                                        window.alert("Join team success!");
+                                         
+                                                        }
+                                                );
+                        
+                                        });
                 
-                    memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
-                        function(oldLocation){
-                            movingMember=oldLocation.val();
-                            eventRef.orderByChild("name").equalTo(event).once("child_added",function(targetEventRef){
-                                targetEventRef.ref.child("Team").orderByChild("name").equalTo(teamName).once("child_added",function(newLocation){
-                                    newLocation.child("member").ref.push().set(movingMember);
-                                    newLocation.ref.update({numberOfmember: newLocation.val().numberOfmember+1});
-                                    oldLocation.ref.remove();
-                                    window.alert("Join team success!");
-
-                                });
-                
-                            });
+                          //      });
                 
                         });
-                
-                
-                
+                }
+                else{
+                    window.alert("Please sign in first!");
+                }    
             
             };
-
-             $scope.test = function (){
-                $state.go('personality');
-                
-
-            };
             
+            
+
 
             $scope.reqJoinTeamInPerson = function (event,teamName){
                 
-              
+                if(firebase.auth().currentUser){//if logged in
                         
                         memberNoTeamRef.orderByChild("uid").equalTo(firebase.auth().currentUser.uid).once("child_added",
                                 function(exist)
@@ -113,7 +250,10 @@ app.controller("joinTeam",
                                         
                                 }                       
                         );
-                   
+                }
+                else{
+                    window.alert("Please sign in first!");
+                }    
             };  
     }                      
 );
