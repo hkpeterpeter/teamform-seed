b024f58d5af1e00fb545098df018890ac2448323
Harry <harry_hong14@hotmail.com>
2016-11-29 08:23:00 +0800

    Modified File

--- TeamFormingSystemDemostration/js/pi.js
+++ TeamFormingSystemDemostration/js/pi.js
@@ -1,9 +1,16 @@
+// inject firebase service
 var app = angular.module("pi", ["firebase"]); 
 
 app.controller("info", 
 
 	// Implementation the todoCtrl 
 	function($scope, $firebaseArray) {
+
+        $scope.iq = {
+            Name : "Kit",
+            iq : 0,
+            eq : 0
+        }
 
 		var ref=firebase.database().ref("pi");
     	ref.orderByChild("Name").equalTo("Kit").once("child_added", function(dataRef) {
@@ -12,6 +19,25 @@ app.controller("info",
         	});
             console.log($scope.level);
         });
+
+        var ref=firebase.database().ref("iq");
+        ref.orderByChild("Name").equalTo("Kit").once("child_added", function(dataRef) {
+            $scope.$apply(function() {
+            $scope.newIq = dataRef.child("iq").val();
+            $scope.newEq = dataRef.child("eq").val();
+            });
+        });
+
+        $scope.updateIq = function() {
+            var path = "iq/-KXhHdaWWvGuSqqIRaMH";
+            var itemRef = firebase.database().ref(path);
+            $scope.newIq = 87;
+            $scope.iq.iq = 87;
+			$scope.newEq = 87;
+			$scope.iq.eq = 87;
+            itemRef.update($scope.iq);
+            $state.go("iqtest");
+        }
 
 	}
 );
