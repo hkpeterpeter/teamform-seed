ce693fd1ef201f4d404ac0be216602ef9c25b0fa
Sam LAM <samlam@neousoft.com>
2016-11-29 11:47:40 +0800

    portal

---
+++ app/js/portal.js
@@ -0,0 +1,42 @@
+
+$("#obj").width($(window).width());
+$("#obj").height($(window).height());
+
+function fixSize() {
+$("#obj").width($(window).width());
+$("#obj").height($(window).height());
+}
+
+$("#search").click(function() {
+    $("#obj").attr("data", "search.html");
+   fixSize(); 
+});
+
+$("#chat").click(function() {
+    $("#obj").attr("data", "Chatroom.html");
+    fixSize();
+});
+
+$("#profile").click(function() {
+        $("#obj").attr("data", "member.html");
+        fixSize();
+});
+
+$("#team").click(function() {
+        $("#obj").attr("data", "team.html");
+        fixSize();
+});
+
+$("#create").click(function() {
+    $("#obj").attr("data", "createGroup.html");
+    fixSize();
+});
+
+$("#logout").click(function() {
+    firebase.auth().signOut().then(function() {
+        var url = "index.html";
+        window.location.href = url;
+    }, function() {
+
+    });
+});
